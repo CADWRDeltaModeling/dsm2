adedfd62b53205cd982ebb9a65edcda1236bf84f
No Author <noauthor@water.ca.gov>
2007-05-09 22:30:02 +0000

    git-svn-id: http://dminfo:8686/svn/repository/studies/trunk@44 cdc4813b-4270-ec4f-936e-925f93a782c2

---
+++ dsm2/studies/sdip/prepro/prep_gates.py
@@ -0,0 +1,47 @@
+##!d:/vista/bin/vscript.bat 
+
+from jnios import os
+import sys,string,time
+
+from config import setConfigVars, getAttr
+import dcc
+import vdss
+from vista.set import DataReference
+#
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        raise SystemExit("""
+        Usage: vscript dsm2.py infile-for-envvars
+        where infile-for-envvars is the input file for env variables
+        (give full path if not in current running shell)
+        """)
+    else:
+        infile = sys.argv[1]
+
+        c=setConfigVars(infile)
+        print 'Creating Delta Cross Channel daily ops from monthly...'
+        dcc.dccOp(
+            getAttr('CALSIMFILE'),              # CALSIM DSS file (input for DSM2)
+            getAttr('GATEFILE'),                # processed gate DSS file (will be input for DSM2)
+            '/CALSIM/DXC/GATE-DAYS-OPEN//1MON//' + getAttr('CALSIMSTUDY') + '/', # CALSIM DXC pathname
+            '/CALSIM-PROCESSED/DCC/OP//IR-YEAR/' +   \
+            getAttr('CALSIMSTUDY') + '/', # processed cross channel pathname
+            0,                                  # 0: CALSIM input is hardwired to 30-day months
+            1,                                  # operate gate between 0 & 1
+            '01JAN1974 0000 - 31DEC1991 2400'   # time window
+            )
+
+        print 'Copying gate ops for Clifton Court'
+        path='/PLANNING\+GATE/CHWST000/OP-FROM-NODE//IR-YEAR/%s/' \
+              % getAttr("CLIFTONCT_GATEOP")
+        f=vdss.opendss(getAttr('CLIFTONCT_GATEFILE')) 
+        g=vdss.findpath(f,path)
+        if ( not g or len(g) != 1):
+            raise "Path not found or not unique: %s" % (path)
+        ts=DataReference.create(g[0]).getData()
+        vdss.writedss(getAttr('GATEFILE'),path.replace("\\",""),ts)
+        print "Finished with clifton court transfer"
+        sys.exit(0)
+#
+
+
