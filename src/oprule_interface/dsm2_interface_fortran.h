a1aa7bed7bb8646f850aa2a564e9fa2d0c2ab3fc
No Author <noauthor@water.ca.gov>
2007-12-11 21:40:07 +0000

    git-svn-id: http://dminfo:8686/svn/repository/models/trunk/dsm2@161 cdc4813b-4270-ec4f-936e-925f93a782c2

---
+++ src/oprule_interface/dsm2_interface_fortran.h
@@ -0,0 +1,185 @@
+// FORTRAN-C interface for operating rules to discover/manipulate
+// FORTRAN model variables using FORTRAN functions/subroutines.
+// This file takes care of naming conventions, the actual FORTRAN functions
+// are either in model_interface or elsewhere in the model source.
+// Note that this file is vendor-specific. If we add/change compilers it
+// will almost certainly need to be changed.
+
+#define get_external_flow GET_EXTERNAL_FLOW
+#define set_external_flow SET_EXTERNAL_FLOW
+#define set_external_flow_datasource SET_EXTERNAL_FLOW_DATASOURCE
+#define get_transfer_flow GET_TRANSFER_FLOW
+#define set_transfer_flow SET_TRANSFER_FLOW
+#define set_transfer_flow_datasource SET_TRANSFER_FLOW_DATASOURCE
+
+
+
+#define set_gate_install SET_GATE_INSTALL
+#define is_gate_install IS_GATE_INSTALL
+#define set_device_op_coef SET_DEVICE_OP_COEF
+#define get_device_op_coef GET_DEVICE_OP_COEF
+#define set_device_op_datasource SET_DEVICE_OP_DATASOURCE
+#define set_device_position SET_DEVICE_POSITION
+#define get_device_position GET_DEVICE_POSITION
+#define set_device_position_datasource SET_DEVICE_POSITION_DATASOURCE
+#define set_device_height SET_DEVICE_HEIGHT
+#define get_device_height GET_DEVICE_HEIGHT
+#define set_device_width SET_DEVICE_WIDTH
+#define get_device_width GET_DEVICE_WIDTH
+#define set_device_elev SET_DEVICE_ELEV
+#define get_device_elev GET_DEVICE_ELEV
+#define set_device_nduplicate SET_DEVICE_NDUPLICATE
+#define get_device_nduplicate GET_DEVICE_NDUPLICATE
+#define set_device_flow_coef SET_DEVICE_FLOW_COEF
+#define get_device_flow_coef GET_DEVICE_FLOW_COEF
+
+#define get_surf_elev GLOBALSTREAMSURFACEELEVATION
+#define get_flow GLOBALSTREAMFLOW
+#define get_res_flow GET_RES_FLOW
+#define get_res_surf_elev GET_RES_SURF_ELEV
+#define ext2int EXT2INT
+#define reservoir_index RESNDX
+#define ext2intnode EXT2INTNODE
+#define qext_index QEXT_INDEX
+#define transfer_index TRANSFER_INDEX
+#define chan_comp_point CHAN_COMP_POINT
+#define gate_index GATENDX
+#define device_index DEVICENDX
+#define ts_index TS_INDEX
+#define get_chan_velocity CHANNELVELOCITY
+#define direct_to_node DIRECT_TO_NODE
+#define direct_from_node DIRECT_FROM_NODE
+#define channel_length CHANNEL_LENGTH
+
+
+// routines for retrieving indexes and converting external
+// numbers to internal ones
+extern "C" int __stdcall gate_index(const char* name, 
+                                    unsigned int len);
+extern "C" int __stdcall device_index(const int& gateno, 
+                                    const char* name, 
+                                    unsigned int len);
+extern "C" int __stdcall ext2int(const int& extchan);
+extern "C" int __stdcall ext2intnode(const int& extres);
+extern "C" int __stdcall reservoir_index(const char* name, 
+                                     unsigned int len);
+
+
+extern "C" void __stdcall chan_comp_point(const int& intchan, 
+                                          const double& distance,
+                                          int points[], 
+                                          double weights[]);
+extern "C" double __stdcall channel_length(const int & intchan);
+
+extern "C" int __stdcall ts_index(const char* name, unsigned int len);
+extern "C" int __stdcall qext_index(const char* name, unsigned int len);
+extern "C" int __stdcall transfer_index(const char* name, unsigned int len);
+
+extern "C" int __stdcall direct_to_node();
+extern "C" int __stdcall direct_from_node();
+
+///////////////////////////
+
+// Model variable interfaces
+
+extern "C" double __stdcall get_external_flow(const int& ndx);
+extern "C" double __stdcall set_external_flow(const int& ndx, 
+                                              const double& val);
+extern "C" double __stdcall set_external_flow_datasource(const int& ndx, 
+                                              const int& expr,
+                                              const double& val,
+                                              const bool& timedep);
+extern "C" double __stdcall get_transfer_flow(const int& ndx);
+extern "C" double __stdcall set_transfer_flow(const int& ndx, 
+                                              const double& val);
+extern "C" double __stdcall set_transfer_flow_datasource(const int& ndx, 
+                                              const int& expr,
+                                              const double& val,
+                                              const bool& timedep);
+
+
+
+
+extern "C" double __stdcall is_gate_install(const int& ndx);
+
+extern "C" double __stdcall set_gate_install(const int& ndx, 
+                                             const double& install);
+/*extern "C" void __stdcall set_gate_install_datasource(const int& ndx, 
+                                                      const int& expr,
+                                                      const int& val,
+                                                      const bool& timedep);
+*/
+extern "C" double __stdcall get_surf_elev(const int& comp_pt);
+extern "C" double __stdcall get_flow(const int& comp_pt);
+extern "C" double __stdcall get_res_flow(const int& resndx, 
+                                         const int& conn);
+extern "C" double __stdcall get_res_surf_elev(const int& resndx);
+extern "C" double __stdcall get_device_op_coef(const int& ndx, 
+                                             const int& devndx,
+											 const int& direct);
+
+extern "C" void __stdcall set_device_op_coef(const int& ndx, 
+                                           const int& devndx,
+										   const int& direct,
+                                           const double& val);
+extern "C" void __stdcall set_device_op_datasource(const int& ndx, 
+                                           const int& devndx,
+										   const int& direct,
+                                           const int& expr,
+                                           const double& val,
+                                           const bool& timedep);
+
+
+extern "C" double __stdcall get_device_position(const int& ndx, 
+                                             const int& devndx);
+
+extern "C" void __stdcall set_device_position(const int& ndx, 
+                                           const int& devndx,
+                                           const double& val);
+extern "C" void __stdcall set_device_position_datasource(const int& ndx, 
+                                           const int& devndx,
+                                           const int& expr,
+                                           const double& val,
+                                           const bool& timedep);
+
+
+extern "C" double __stdcall get_device_height(const int& ndx, 
+                                             const int& devndx);
+
+extern "C" void __stdcall set_device_height(const int& ndx, 
+                                           const int& devndx,
+                                           const double& val);
+
+extern "C" double __stdcall get_device_elev(const int& ndx, 
+                                             const int& devndx);
+
+extern "C" void __stdcall set_device_elev(const int& ndx, 
+                                           const int& devndx,
+                                           const double& val);
+
+extern "C" double __stdcall get_device_width(const int& ndx, 
+                                             const int& devndx);
+
+extern "C" void __stdcall set_device_width(const int& ndx, 
+                                           const int& devndx,
+                                           const double& val);
+
+extern "C" double __stdcall get_device_nduplicate(const int& ndx, 
+                                             const int& devndx);
+
+extern "C" void __stdcall set_device_nduplicate(const int& ndx, 
+                                           const int& devndx,
+                                           const double& val);
+
+extern "C" double __stdcall get_device_flow_coef(const int& ndx, 
+                                             const int& devndx,
+											 const int& direction);
+
+extern "C" void __stdcall set_device_flow_coef(const int& ndx, 
+                                           const int& devndx,
+										   const int& direction,
+                                           const double& val
+										   );
+
+
+extern "C" double __stdcall get_chan_velocity(const int&, const double&);
---
+++ src/oprule_interface/dsm2_oprule_management.cpp
@@ -0,0 +1,79 @@
+#include <string>
+#include <iostream>
+#include "oprule/expression/ExpressionNode.h" 
+#include "oprule/parser/NamedValueLookup.h"
+#include "oprule/parser/ModelTimeNodeFactory.h"
+#include "oprule/parser/ModelActionFactory.h"
+#include "oprule/rule/OperatingRule.h"
+#include "oprule/rule/OperationManager.h"
+#include "oprule/parser/ParseResultType.h"
+#include "oprule/rule/ModelInterfaceActionResolver.h"
+
+#include "dsm2_named_value_lookup.h"
+#include "dsm2_time_node_factory.h"
+#include "dsm2_model_action_factory.h"
+#include "dsm2_time_interface.h"
+#include "dsm2_model_interface_resolver.h"
+
+using namespace oprule::rule;
+using namespace oprule::parser;
+using namespace oprule::expression;
+using namespace std;
+
+extern parse_type get_parsed_type();
+extern void init_expression();
+extern void lexer_init();
+extern void add_expression(const string& name, 
+                           oprule::expression::DoubleNodePtr expr);
+extern void init_lookup( NamedValueLookup*);
+extern void init_model_time_factory(ModelTimeNodeFactory * );
+extern void init_action_factory(ModelActionFactory *);
+extern void set_input_string(string &);
+extern void set_input_string(char*);
+extern int op_ruleparse();
+extern OperatingRule* getOperatingRule();
+
+
+#define init_parser_f __stdcall INIT_PARSER_F
+#define advance_op_rules __stdcall ADVANCEOPRULES
+#define test_rule_activation __stdcall TESTOPRULEACTIVATION
+#define parse_rule __stdcall PARSE_RULE
+
+ModelInterfaceActionResolver<DSM2ModelTimer, DSM2Resolver,
+                             DSM2ModelInterfaceResolver> resolver;
+OperationManager dsm2_op_manager(resolver);
+
+using namespace std;
+extern "C"{
+void init_parser_f(){
+     lexer_init();
+     init_expression();
+     init_lookup(new DSM2HydroNamedValueLookup());
+     init_model_time_factory(new  DSM2HydroTimeNodeFactory());
+     init_action_factory(new dsm2_model_action_factory());
+}
+
+bool advance_op_rules(){
+   dsm2_op_manager.advanceStep();
+   return true;
+}
+
+bool test_rule_activation(){
+   dsm2_op_manager.manageActivation();
+   return true;
+}
+
+bool parse_rule(char* text, int len){
+   string parse_str(text,len);
+   set_input_string(parse_str);
+   int parseok=op_ruleparse(); // make sure it was a rule??
+   if( !(parseok==0) || get_parsed_type() == oprule::parser::PARSE_ERROR)return false;
+   if (get_parsed_type() == oprule::parser::OP_RULE){
+     OperatingRule * rule=getOperatingRule();
+     dsm2_op_manager.addRule(rule);
+   }
+   return true;
+}
+}
+
+
