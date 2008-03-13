0f85ac4fe3d4ccbcf00168caa91719329c68b062
No Author <noauthor@water.ca.gov>
2008-03-13 21:46:11 +0000

    Copied over latest version of scripts from the old sdip/prepro directory
    git-svn-id: http://dminfo:8686/svn/repository/studies/trunk@266 cdc4813b-4270-ec4f-936e-925f93a782c2

--- dsm2/scripts/prep_ec.py
+++ dsm2/scripts/prep_ec.py
@@ -1,23 +1,35 @@
 from jnios import os
 import expand_seasonal
+import planning_ec_mtz
+import planning_ec_vernalis
 import sys,string
 import config
 import interpolate
 from vtimeseries import timewindow
-from vdss import findpath,opendss,writedss
 from vista.set import DataReference
 import config
 #
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         raise SystemExit("""
-        Usage: vscript prepEC.py configfile
+        Usage: vscript prep_ec.py configfile
         where configfile is the input file for configuration variables
         (give full path if not in current running shell)
         """)
     else:
+        print "IN prep EC"
         infile = sys.argv[1]
         config.setConfigVars(infile)
+        tws=config.getAttr('START_DATE')+ " 0000 - " + \
+            config.getAttr('END_DATE') + " 2400"
+        tw=timewindow(tws)
         print "Expanding seasonal DICU WQ drainage values"
-        expand_seasonal.mainExpandSeasonal()
-    sys.exit()
+        expand_seasonal.prep_dicu(
+            config.getAttr('DICUFILE_EC'),        # original DICU DSS file for EC
+            config.getAttr('DICUFILE_ECE'),       # processed DICU DSS file (will be input for DSM2)
+            tw)
+        planning_ec_mtz.planning_ec_mtz()
+        planning_ec_vernalis.transfer_ec()  #direct copy of planning ec, no vamp
+        sys.exit()
+
+
