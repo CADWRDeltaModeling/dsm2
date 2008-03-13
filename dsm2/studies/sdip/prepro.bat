ea979457ec6cdee3cc3731a70d48ca018d78a35a
No Author <noauthor@water.ca.gov>
2008-03-13 21:46:56 +0000

    Copied over latest version of scripts from the old sdip/prepro directory
    git-svn-id: http://dminfo:8686/svn/repository/studies/trunk@267 cdc4813b-4270-ec4f-936e-925f93a782c2

--- dsm2/studies/sdip/prepro.bat
+++ dsm2/studies/sdip/prepro.bat
@@ -14,9 +14,9 @@ rem do not add spaces to the following command
 if NOT EXIST %CONFIGFILE% GOTO noconfig
 echo Prepro is needed only when the CALSIM file changes.
 call vscript ../../scripts/planning_boundary_flow.py %CONFIGFILE%
-call vscript prepro/prep_gates.py %CONFIGFILE%
+call vscript ../../scripts/prep_gates.py %CONFIGFILE%
 call vscript ../../scripts/prep_vamp.py %CONFIGFILE%
-call vscript prepro/prep_ec.py %CONFIGFILE%
+call vscript ../../scripts/prep_ec.py %CONFIGFILE%
 )
 
 goto fin
