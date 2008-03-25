7cb1ad7215d88b1e58e883a4a839b5462ac499ae
No Author <noauthor@water.ca.gov>
2008-03-25 21:17:00 +0000

    git-svn-id: http://dminfo:8686/svn/repository/models/trunk/dsm2_distribute@313 cdc4813b-4270-ec4f-936e-925f93a782c2

---
+++ dsm2/ptm/bin/ptm.bat
@@ -0,0 +1,23 @@
+@echo off
+
+set ptm_home=%~dp0%../
+
+if exist %ptm_home%/lib/ptm.jar goto :valid
+
+
+:notfound
+
+echo ## An Error has occured ##
+PAUSE
+goto :end
+
+:valid
+
+set path=%ptm_home%/bin;%ptm_home%/lib;%path%
+
+java -ss1m -mx128m -oss1m -classpath "%ptm_home%/lib/ptm.jar;%ptm_home%/lib/COM.jar;%ptm_home%/lib/edu.jar;%ptm_home%/lib/xml.jar" DWR.DMS.PTM.mainPTM %1
+rem java -ss1m -mx128m -classpath "%ptm_home%/classes;%ptm_home%/lib/COM.jar;%ptm_home%/lib/edu.jar;%ptm_home%/lib/xml.jar" DWR.DMS.PTM.mainPTM %1
+
+:end
+
+
