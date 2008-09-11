9dca2dc3fc6f06d3e67126d3de4b922923129891
No Author <noauthor@water.ca.gov>
2008-09-11 19:19:34 +0000

    git-svn-id: http://dminfo:8686/svn/repository/models/trunk/dsm2_distribute@425 cdc4813b-4270-ec4f-936e-925f93a782c2

--- dsm2/scripts/version.py
+++ dsm2/scripts/version.py
@@ -1,6 +1,12 @@
 ##!d:/vista/bin/vscript.bat 
 
-print "$Revision$"
+def version():
+    f = open("version.txt",'r')
+    version = version.readline()
+    return version
+
+if __name__ == "__main__":
+    print version()
 
 
 
