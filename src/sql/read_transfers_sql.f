939b9a77cef966b8a3827412e30dfdb824be2f53
No Author <noauthor@water.ca.gov>
2009-02-06 01:14:48 +0000

    git-svn-id: http://dminfo:8686/svn/repository/models/trunk/dsm2@525 cdc4813b-4270-ec4f-936e-925f93a782c2

--- src/sql/read_transfers_sql.f
+++ src/sql/read_transfers_sql.f
@@ -17,7 +17,6 @@ C!    You should have received a copy of the GNU General Public !<license
 C!    along with DSM2.  If not, see <http://www.gnu.org/!<licenses/>.
 C!</license>
 
-
       subroutine load_transfers_SQL(StmtHndl, ModelID, istat)
 
 c-----load f90SQL modules
@@ -38,7 +37,7 @@ c-----arguments
 c-----f90SQL variables
       character(len=1000)::StmtStr
       integer(SQLRETURN_KIND)::iRet
-      integer(SQLSMALLINT_KIND)::ColNumber ! SQL table column number
+      integer(SQLSMALLINT_KIND)::ColNumber = 0 ! SQL table column number
 
 c-----local variables
       integer UseObj
@@ -138,39 +137,12 @@ c--------use only the last version of a transfer, and skip
 c--------if the transfer is marked as not-use
          if (TransName .ne. prevName .and.
      &        UseObj) then
-            nobj2obj=nobj2obj+1
-            if (nobj2obj .gt. max_obj2obj) then
-               write(unit_error,630)
-     &              'Too many object connections specified; max allowed is:'
-     &              ,max_obj2obj
-               istat=-1
-            endif
-            obj2obj(nobj2obj).Use=.true.
-            obj2obj(nobj2obj).ID=ID
-            obj2obj(nobj2obj).name=TransName
-            obj2obj(nobj2obj).from_obj.obj_type=FromObjType
-            if (FromObjType .eq. obj_reservoir) then
-               obj2obj(nobj2obj).from_obj.obj_name=FromObjID
-               obj2obj(nobj2obj).from_obj.obj_no=name_to_objno(obj_reservoir,FromObjID)
-            else if (FromObjType .eq. obj_node) then
-               read(FromObjID,'(i10)')obj2obj(nobj2obj).from_obj.obj_no
-               obj2obj(nobj2obj).from_obj.obj_no=
-     &           ext2intnode(obj2obj(nobj2obj).from_obj.obj_no)
-               obj2obj(nobj2obj).from_obj.obj_name=FromObjID
-            end if
-            obj2obj(nobj2obj).to_obj.obj_type=ToObjType
-            if (ToObjType .eq. obj_reservoir) then
-               obj2obj(nobj2obj).to_obj.obj_name=ToObjID
-               obj2obj(nobj2obj).to_obj.obj_no=name_to_objno(obj_reservoir,ToObjID)
-            else if (ToObjType .eq. obj_node) then
-               read(ToObjID,'(i10)')obj2obj(nobj2obj).to_obj.obj_no
-               obj2obj(nobj2obj).to_obj.obj_no=
-     &              ext2intnode(obj2obj(nobj2obj).to_obj.obj_no)
-               obj2obj(nobj2obj).to_obj.obj_name=ToObjID
-            end if
-            if (print_level .ge. 3)
-     &           write(unit_screen,'(i5,a)') nobj2obj,
-     &           trim(obj2obj(nobj2obj).name)
+            call process_transfer(ID,
+     &                            TransName,
+     &                            FromObjType,
+     &                            FromObjID,
+     &                            ToObjType,
+     &                            ToObjID)
          endif
          PrevName=TransName
       enddo
@@ -190,7 +162,5 @@ c--------if the transfer is marked as not-use
      &        write(unit_screen,'(a//)') 'Unbound transfer SQL'
       endif
 
- 630  format(/a,i5)
-
       return
-      end
+      end subroutine
