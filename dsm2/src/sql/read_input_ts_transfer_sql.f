b81e7b9858864c50db8e8170df5bcb8dc06988dc
No Author <noauthor@water.ca.gov>
2009-02-18 22:33:34 +0000

    git-svn-id: http://dminfo:8686/svn/repository/models/trunk@585 cdc4813b-4270-ec4f-936e-925f93a782c2

--- dsm2/src/sql/read_input_ts_transfer_sql.f
+++ dsm2/src/sql/read_input_ts_transfer_sql.f
@@ -142,110 +142,33 @@ c--------clean up name variable, replace environment variables
          call locase(Name)
          LocName = trim(Name)  ! Special case for transfer
 
-
          Param=Param(1:ParamLen)
          nenv=replace_envvars(Param,ctmp)
          Param=ctmp
          call locase(Param)
 
+         InPath=InPath(1:PathLen)
+         nenv=replace_envvars(InPath,ctmp)
+         InPath=ctmp
+         call locase(InPath)
+
+         FileName=FileName(1:filelen) ! preserve case for filename
+         nenv=replace_envvars(FileName,ctmp)
+         FileName=ctmp
+
+
+
 c--------use only the highest layer version of the input, and skip
 c--------if marked as not-use
          if ( (.not.(Name .eq. PrevName .and. Param .eq. PrevParam))
      &        .and. UseObj) then
-            ninpaths=ninpaths+1
-            if (ninpaths .gt. max_inputpaths) then
-               write(unit_error,630)
-     &              'Too many input paths specified; max allowed is:'
-     &              ,max_inputpaths
-               istat=-1
-               return
-            endif
 
-c-----------clean up character variables, replace environment variables
-            InPath=InPath(1:PathLen)
-            nenv=replace_envvars(InPath,ctmp)
-            InPath=ctmp
-            call locase(InPath)
+            call process_input_transfer(Name,
+     &                                  InPath,
+     &                                  Param,
+     &                                  Fillin,
+     &                                  Filename)
 
-            FileName=FileName(1:filelen) ! preserve case for filename
-            nenv=replace_envvars(FileName,ctmp)
-            FileName=ctmp
-
-            pathinput(ninpaths).name=Name
-            pathinput(ninpaths).useobj=.true.
-            pathinput(ninpaths).obj_name=LocName
-            pathinput(ninpaths).obj_type=ObjTypeID
-
-c-----------find object number given external object number
-            pathinput(ninpaths).obj_name=trim(LocName)
-            pathinput(ninpaths).obj_no=name_to_objno(ObjTypeID,LocName)
-            if (pathinput(ninpaths).obj_no .eq.miss_val_i )then
-               write(unit_error,'(a,a)')
-     &              'Time Series Input: ',trim(pathinput(ninpaths).name),
-     &              ' attached to unrecognized object: ',LocName
-               istat=-3
-               return
-            end if
-
-            if (FileName(:8) .eq. 'constant' .or.
-     &             FileName(:8) .eq. 'CONSTANT') then
-               read(InPath,'(1f10.0)') ftmp
-               pathinput(ninpaths).constant_value=ftmp
-               pathinput(ninpaths).variable=Param
-               pathinput(ninpaths).fillin=fill_last
-            else
-c--------------Break up the input pathname
-
-               pathinput(ninpaths).path=trim(InPath)
-               call chrlnb(InPath, npath)
-               call zufpn(ca, na, cb, nb, cc, nc, cd, nd, ce, ne,
-     &              cf, nf, InPath, npath, istat)
-               if (istat .lt. 0) then
-                  write(unit_error, '(/a/a)')
-     &                 'Input TS: Illegal pathname', InPath
-                  istat = -1
-                  return
-               end if
-
-               pathinput(ninpaths).variable=Param
-               call split_epart(ce,itmp,ctmp)
-               if (itmp .ne. miss_val_i) then ! valid interval, parse it
-                  pathinput(ninpaths).no_intervals=itmp
-                  pathinput(ninpaths).interval=ctmp
-               else
-                  write(unit_error,610)
-     &                 'Input TS: Unknown input E part or interval: ' // ce
-                  write(unit_error,'(a)') 'Path: ' // trim(InPath)
-                  istat=-1
-                  return
-               endif
-               pathinput(ninpaths).filename=FileName
-c--------------accumulate unique dss input filenames
-               itmp=loccarr(pathinput(ninpaths).filename,infilenames,
-     &              max_dssinfiles, EXACT_MATCH)
-               if (itmp .lt. 0) then
-                  if (abs(itmp) .le. max_dssinfiles) then
-                     infilenames(abs(itmp))=pathinput(ninpaths).filename
-                     pathinput(ninpaths).ndx_file=abs(itmp)
-                  else
-                     write(unit_error,610)
-     &                    'Maximum number of unique DSS input files exceeded'
-                     istat=-3
-                     return
-                  endif
-               else
-                  pathinput(ninpaths).ndx_file=itmp
-               endif
-               pathinput(ninpaths).fillin=Fillin
-            endif
-
-c-----------set data type
-            pathinput(ninpaths).data_type=obj_obj2obj
-            if (print_level .ge. 3)then
-               write(unit_screen,'(i4,1x,a32,1x,a24,a24)') ninpaths, Name,
-     &              trim(InPath(:24)),
-     &              trim(FileName(:24))
-            end if
          end if                 !inputpath name not equals last name processed
          counter=counter+1
          prevName=Name
