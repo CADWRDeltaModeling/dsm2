0c5064c9e358cf1909538390d4addd0cb23ef9b9
No Author <noauthor@water.ca.gov>
2009-02-18 23:17:30 +0000

    git-svn-id: http://dminfo:8686/svn/repository/models/trunk@590 cdc4813b-4270-ec4f-936e-925f93a782c2

--- dsm2/src/fixed/process_input_climate.f
+++ dsm2/src/fixed/process_input_climate.f
@@ -1,3 +1,22 @@
+C!<license>
+C!    Copyright (C) 1996, 1997, 1998, 2001, 2007 State of California,
+C!    Department of Water Resources.
+C!    This file is part of DSM2.
+
+C!    DSM2 is free software: you can redistribute it and/or modify
+C!    it under the terms of the GNU General Public !<license as published by
+C!    the Free Software Foundation, either version 3 of the !<license, or
+C!    (at your option) any later version.
+
+C!    DSM2 is distributed in the hope that it will be useful,
+C!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+C!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+C!    GNU General Public !<license for more details.
+
+C!    You should have received a copy of the GNU General Public !<license
+C!    along with DSM2.  If not, see <http://www.gnu.org/!<licenses/>.
+C!</license>
+
       subroutine process_input_climate(Name,
      &                                 InPath,
      &                                 Param,
@@ -42,8 +61,7 @@
                write(unit_error,630)
      &              'Too many input paths specified; max allowed is:'
      &              ,max_inputpaths
-               istat=-1
-               return
+               call exit(-1)
             endif
 
             pathinput(ninpaths).name=Name
@@ -80,8 +98,7 @@ c--------------Break up the input pathname
                if (istat .lt. 0) then
                   write(unit_error, '(/a/a)')
      &                 'Input TS: Illegal pathname', InPath
-                  istat = -1
-                  return
+                  call exit(-1)
                end if
 
                pathinput(ninpaths).variable=Param
@@ -93,8 +110,7 @@ c--------------Break up the input pathname
                   write(unit_error,610)
      &                 'Input TS: Unknown input E part or interval: ' // ce
                   write(unit_error,'(a)') 'Path: ' // trim(InPath)
-                  istat=-1
-                  return
+                  call exit(-1)
                endif
                pathinput(ninpaths).filename=FileName
 c--------------accumulate unique dss input filenames
@@ -107,8 +123,7 @@ c--------------accumulate unique dss input filenames
                   else
                      write(unit_error,610)
      &                    'Maximum number of unique DSS input files exceeded'
-                     istat=-3
-                     return
+                     call exit(-3)
                   endif
                else
                   pathinput(ninpaths).ndx_file=itmp
