c4cbda96139a7bb0b661aeffa360598e1cf618a3
No Author <noauthor@water.ca.gov>
2009-02-26 07:23:25 +0000

    Testing for comment after whitespace, plus updated doxygen.config.
    git-svn-id: http://dminfo:8686/svn/repository/models/trunk@629 cdc4813b-4270-ec4f-936e-925f93a782c2

--- input_storage/doc/document.bat
+++ input_storage/doc/document.bat
@@ -1,4 +1,5 @@
 echo off
+::dot.exe || echo "dot.exe must be on path"
 set DOCFORT=doc.f90
 del %DOCFORT%
 echo ! This file is for documentation purposes only. It is a workaround because doxygen cannot handle include files > doc.f90
