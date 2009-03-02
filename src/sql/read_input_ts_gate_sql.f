c8de71aa5de18ca1c0d22e8c3414abbb7d28220b
No Author <noauthor@water.ca.gov>
2009-03-02 19:00:45 +0000

    fixed mismatched character length.
    git-svn-id: http://dminfo:8686/svn/repository/models/trunk/dsm2@671 cdc4813b-4270-ec4f-936e-925f93a782c2

--- src/sql/read_input_ts_gate_sql.f
+++ src/sql/read_input_ts_gate_sql.f
@@ -73,12 +73,12 @@ c-----local variables
       character
      &     InPath*80
      &     ,FileName*128
-     &     ,Param*32
-     &     ,PrevParam*32
+     &     ,Param*16
+     &     ,PrevParam*16
      &     ,LocName*32
      &     ,SubLoc*32           ! Object-dependent sublocation (gate device, reservoir node connect..)
      &     ,PrevName*32
-     &     ,Name*64
+     &     ,Name*32
      &     ,ca*32, cb*32, cc*32, cd*32, ce*32, cf*32
      &     ,ctmp*200
 
@@ -210,8 +210,8 @@ c--------if marked as not-use
 
             call process_input_gate(LocName,
      &                              SubLoc,
-     &                              InPath,
      &                              Param,
+     &                              InPath,
      &                              Fillin,
      &                              Filename) 
 
