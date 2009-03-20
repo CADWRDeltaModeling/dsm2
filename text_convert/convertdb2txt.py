e6d9555be27c3dca6aad3f9c3c47d0c06e0c2239
No Author <noauthor@water.ca.gov>
2009-03-20 00:10:07 +0000

    Improved some queries. Currently runs top to bottom.
    git-svn-id: http://dminfo:8686/svn/repository/models/trunk/dsm2@747 cdc4813b-4270-ec4f-936e-925f93a782c2

--- text_convert/convertdb2txt.py
+++ text_convert/convertdb2txt.py
@@ -5,7 +5,7 @@ from component import *
 # Map of DB input types to the text tables that might contain data of this type
 INPUT_TYPE_TXT_PARENT_TABLES={"grid":["channel","gate","reservoir","transfer","channel_ic","reservoir_ic"],\
                               "input":["input_climate","input_transfer_flow",\
-                              "input_gate","input_node","boundary_stage",\
+                              "input_gate","boundary_stage",\
                               "boundary_flow","source_flow","source_flow_reservoir",\
                               "node_concentration","reservoir_concentration"],
                               "output":["output_channel","output_reservoir",\
@@ -34,7 +34,6 @@ SQL={"channel":channelSQL,\
                "input_climate":inputclimateSQL,\
                "input_transfer_flow":inputtransferflowSQL,\
                "input_gate":inputgateSQL,\
-               "input_node":inputnodeSQL,\
                "boundary_stage":boundarystageSQL,\
                "boundary_flow":boundaryflowSQL,\
                "source_flow":sourceflowSQL,\
@@ -59,6 +58,11 @@ SQL={"channel":channelSQL,\
 
 COMPONENT_MEMBERS=component_members()
 
+def channel_ic_convert(data):
+    new_data=[str(field) for field in data]
+    if data[1] < -1:
+        new_data[1]="length"
+    return new_data
 
 def trivial_convert(row):
     return [str(field) for field in row]
@@ -73,18 +77,28 @@ def quote_string_convert(row):
     new_row=[str(field) for field in row]
     new_row=[quote_string(field) for field in new_row]
     
+def quote_string_drop_interior_quote_converter(row):
+    new_row=[str(field).replace("\"","") for field in row]
+    new_row=[str(field).replace("\'","") for field in row]    
+    new_row=[quote_string(field) for field in new_row]
+    
 CONVERTERS={"channel_ic" : channel_ic_convert,
                              "oprule": quote_string_convert,
                              "oprule_expressions": quote_string_convert,
                              }
 
-def convert_table(fout,tablename,layerid):
+def convert_table(filename,append,tablename,layerid):
         print "Converting table: %s\n" % tablename
         sql=SQL[tablename]
         data=cur.execute(sql,layerid).fetchall()
         if not data or (len(data) ==0): 
             print "Table \"%s\" empty" % tablename
-            return
+            return        
+        if append:
+            fout=open(filename,"a")
+        else:
+            fout=open(filename,"w")
+
         header=COMPONENT_MEMBERS[tablename]
         fout.write(tablename.upper())
         if CONVERTERS.has_key(tablename):
@@ -97,7 +111,8 @@ def convert_table(fout,tablename,layerid):
             datastr=converter(row)
             rowtxt=string.join(datastr,"        ")
             fout.write("%s\n" % rowtxt)
-        fout.write("END\n\n##\n")    
+        fout.write("END\n\n##\n")
+        fout.close()
 
 def convert_layer(db_name,cur,txt_name,group_by="parent_table"):
     """
@@ -115,12 +130,10 @@ def convert_layer(db_name,cur,txt_name,group_by="parent_table"):
         if txt_parent in TXT_CHILD_TABLES.keys():
             txt_child_list=TXT_CHILD_TABLES[txt_parent]
         fname=txt_parent+"_"+txt_name+".inp"
-        fout=open(fname,"w")
-        convert_table(fout,txt_parent,layerid)
+        convert_table(fname,False,txt_parent,layerid)
         for txt_child in txt_child_list:
-             convert_table(fout,txt_child,layerid)
-        fout.close()
-        
+             convert_table(fname,True,txt_child,layerid)
+
         
 
 
@@ -132,10 +145,10 @@ def get_component_type(db_layer_name,cur):
     return type
 
 if __name__ == "__main__":
-    dbcnn=DBConnect("dsm2input")
+    dbcnn=DBConnect("dsm2input","dsmtwo","User2Dmin")
     cur=dbcnn.cnn.cursor()
-    DB_LAYER_NAME=["std_delta_grid","delta_historical_stage"]
-    for layer in DB_LAYER_NAME:
+    db_layer_name=["std_delta_grid","delta_historical_stage","delta_historical_flow","std_output_hydro_named","std_output_qual_named"]
+    for layer in db_layer_name:
         convert_layer(layer,cur,layer)
 
     cur.close()
