8dee7d25d9b85c48e905ff53788878fa0456535d
No Author <noauthor@water.ca.gov>
2009-03-27 06:01:58 +0000

    git-svn-id: http://dminfo:8686/svn/repository/models/trunk/dsm2@759 cdc4813b-4270-ec4f-936e-925f93a782c2

--- text_convert/sqlquery.py
+++ text_convert/sqlquery.py
@@ -1,14 +1,16 @@
 channelSQL=\
-"""SELECT channel_number,length_ft,manning,dispersion, up_node ,down_node 
-FROM channel WHERE layer_id=?;"""
+"""SELECT channel_number,length_ft,manning,dispersion, up_node ,down_node, used 
+FROM channel WHERE layer_id=?;
+"""
+
 gateSQL=\
-"""SELECT gate.name,object_type_description.name,gate.obj_connected_identifier,gate.node_connected 
+"""SELECT gate.name,object_type_description.name,gate.obj_connected_identifier,gate.node_connected, used 
 FROM gate, object_type_description 
 WHERE object_type_description.object_type_id=gate.obj_connected_type AND gate.layer_id=?;"""
 
 channelicSQL=\
 """
-SELECT ci1.channel_number, ci1.distance, ci1.initial_value, ci2.initial_value
+SELECT ci1.channel_number, ci1.distance, ci1.initial_value, ci2.initial_value, used
 FROM channel_init_condition ci1, channel_init_condition ci2
 WHERE ci1.channel_number=ci2.channel_number
 AND ci1.distance=ci2.distance 
@@ -31,7 +33,7 @@ gatedeviceSQL=\
 SELECT gate.name, gate_device.name, gate_structure_description.name, 
 nduplicate, max_width, base_elev, height, 
 flow_coef_to_node, flow_coef_from_node, 
-gate_default_op_description.name, gate_control_type_description.name 
+gate_default_op_description.name, gate_control_type_description.name
 FROM gate_device, gate, gate_structure_description, gate_control_type_description, gate_default_op_description 
 WHERE gate.gate_id=gate_device.gate_id 
 AND gate_device.structure_type= gate_structure_description.structure_type_id 
@@ -41,7 +43,7 @@ ORDER BY gate.name, gate_device.name;"""
 
 #######
 reservoirSQL=\
-"""SELECT name, area, bottom_elev 
+"""SELECT name, area, bottom_elev, used
 FROM reservoir 
 WHERE layer_id=?
 ORDER BY name"""
@@ -58,7 +60,7 @@ ORDER BY reservoir.name,reservoir_connections.connected_node_number;
 
 reservoiricSQL=\
 """
-SELECT reservoir_name, initial_value
+SELECT reservoir_name, initial_value, used
 FROM reservoir_init_condition
 WHERE layer_id=?
 ORDER BY reservoir_name;
@@ -67,7 +69,7 @@ ORDER BY reservoir_name;
 #######
 transferSQL=\
 """
-SELECT t.name, obj1.name,t.from_object_identifier,obj2.name,t.to_object_identifier
+SELECT t.name, obj1.name,t.from_object_identifier,obj2.name,t.to_object_identifier, used
 FROM transfer t, object_type_description obj1, object_type_description obj2
 WHERE t.from_object_type=obj1.object_type_id AND t.to_object_type=obj2.object_type_id
 AND t.layer_id=?
@@ -77,7 +79,7 @@ ORDER BY t.name;
 
 inputclimateSQL=\
 """
-SELECT  climate.name, input_file,path,fill.name 
+SELECT  climate.name, input_file,path,fill.name, used 
 FROM fill_in_type_description fill, climate_variable_description climate, input_time_series_climate inp
 WHERE layer_id =? AND inp.fillin = fill.fill_in_type_id 
 AND inp.climate_variable_id=climate.climate_variable_id
@@ -86,7 +88,7 @@ ORDER BY  climate.name;
 
 inputtransferflowSQL=\
 """
-SELECT  inp.transfer, input_file,path,fill.name
+SELECT  inp.transfer, input_file,path,fill.name, used
 FROM input_time_series_transfer inp,fill_in_type_description fill
 WHERE layer_id=? AND inp.fillin = fill.fill_in_type_id
 ORDER BY  inp.transfer;
@@ -94,7 +96,7 @@ ORDER BY  inp.transfer;
 
 inputgateSQL=\
 """
-SELECT  inp.gate, inp.device,variable_name,input_file,path,fill.name
+SELECT  inp.gate, inp.device,variable_name,input_file,path,fill.name, used
 FROM input_time_series_gate inp,fill_in_type_description fill
 WHERE layer_id=? AND inp.fillin = fill.fill_in_type_id
 ORDER BY  inp.gate, inp.device, inp.variable_name;
@@ -103,7 +105,7 @@ ORDER BY  inp.gate, inp.device, inp.variable_name;
 
 boundarystageSQL=\
 """
-SELECT  inp.name, node,fill.name,input_file,path
+SELECT  inp.name, node,fill.name,input_file,path, used
 FROM input_time_series_node inp,fill_in_type_description fill
 WHERE role_id=1 AND layer_id=? AND inp.fillin = fill.fill_in_type_id
 ORDER BY  inp.name;
@@ -111,7 +113,7 @@ ORDER BY  inp.name;
 
 boundaryflowSQL=\
 """
-SELECT  input_time_series_node.name,node,sign,fill_in_type_description.name,input_file,path
+SELECT  input_time_series_node.name,node,sign,fill_in_type_description.name,input_file,path, used
 FROM input_time_series_node,fill_in_type_description
 WHERE role_id=2 AND layer_id=? 
 AND input_time_series_node.fillin = fill_in_type_description.fill_in_type_id
@@ -121,7 +123,7 @@ ORDER BY input_time_series_node.name;
 
 sourceflowSQL=\
 """
-SELECT input_time_series_node.name,node,sign,fill_in_type_description.name,input_file,path
+SELECT input_time_series_node.name,node,sign,fill_in_type_description.name,input_file,path, used
 FROM input_time_series_node,fill_in_type_description
 WHERE role_id=4 AND layer_id=? AND variable_name LIKE 'flow'
 AND input_time_series_node.fillin = fill_in_type_description.fill_in_type_id
@@ -132,7 +134,7 @@ ORDER BY input_time_series_node.name;
 
 sourceflowreservoirSQL=\
 """
-SELECT t.name,reservoir,sign,f.name,input_file,path
+SELECT t.name,reservoir,sign,f.name,input_file,path, used
 FROM input_time_series_reservoir t, fill_in_type_description f
 WHERE role_id=4 AND layer_id=? AND variable_name LIKE 'flow'
 AND t.fillin = f.fill_in_type_id
@@ -141,7 +143,7 @@ ORDER BY t.name,reservoir;
 
 nodeconcSQL=\
 """
-SELECT inp.name,node,variable_name,fill.name,input_file,path
+SELECT inp.name,node,variable_name,fill.name,input_file,path, used
 FROM input_time_series_node inp,fill_in_type_description fill
 WHERE (role_id=4 OR role_id=2) AND layer_id=? AND (NOT variable_name LIKE 'flow')
 AND inp.fillin = fill.fill_in_type_id
@@ -149,7 +151,7 @@ ORDER BY inp.name;
 """
 
 reservoirconcSQL=\
-"""SELECT t.name,reservoir,variable_name,f.name,input_file,path
+"""SELECT t.name,reservoir,variable_name,f.name,input_file,path, used
 FROM input_time_series_reservoir t, fill_in_type_description f
 WHERE role_id=4 AND layer_id=? AND (NOT variable_name LIKE 'flow')
 AND t.fillin = f.fill_in_type_id
@@ -158,7 +160,7 @@ ORDER BY t.name,reservoir;
   
 outputchannelSQL=\
 """
-SELECT o.name,o.channel,distance,variable_name,time_interval,period_op,output_file
+SELECT o.name,o.channel,distance,variable_name,time_interval,period_op,output_file, used
 FROM output_time_series_channel o
 WHERE o.layer_id=?
 ORDER BY o.name,variable_name,time_interval;
@@ -166,7 +168,7 @@ ORDER BY o.name,variable_name,time_interval;
 
 outputresSQL=\
 """
-SELECT o.name,o.reservoir,connection_node,variable_name,time_interval,period_op,output_file
+SELECT o.name,o.reservoir,connection_node,variable_name,time_interval,period_op,output_file, used
 FROM output_time_series_reservoir o
 WHERE o.layer_id=?
 ORDER BY o.name,variable_name,time_interval;
@@ -174,7 +176,7 @@ ORDER BY o.name,variable_name,time_interval;
 
 outputchannelconcSQL=\
 """
-SELECT o.name,o.channel,distance,variable_name,source_group,time_interval,period_op,output_file
+SELECT o.name,o.channel,distance,variable_name,source_group,time_interval,period_op,output_file, used
 FROM output_time_series_channel o
 WHERE o.layer_id=?
 ORDER BY o.name,variable_name,source_group,time_interval;
@@ -183,7 +185,7 @@ ORDER BY o.name,variable_name,source_group,time_interval;
 
 outputresconcSQL=\
 """
-SELECT o.name,o.reservoir,variable_name,source_group,time_interval,period_op,output_file
+SELECT o.name,o.reservoir,variable_name,source_group,time_interval,period_op,output_file, used
 FROM output_time_series_reservoir o
 WHERE o.layer_id=?
 ORDER BY o.name,variable_name,source_group,time_interval;
@@ -191,7 +193,7 @@ ORDER BY o.name,variable_name,source_group,time_interval;
 
 outputgateSQL=\
 """
-SELECT o.name,gate,device,variable_name,time_interval,period_op,output_file
+SELECT o.name,gate,device,variable_name,time_interval,period_op,output_file, used
 FROM output_time_series_gate o
 WHERE o.layer_id=?
 ORDER BY o.name,variable_name,time_interval;
@@ -200,7 +202,7 @@ ORDER BY o.name,variable_name,time_interval;
 
 opruleSQL=\
 """
-SELECT t.name,oprule_action,oprule_trigger
+SELECT t.name,oprule_action,oprule_trigger, used
 FROM operating_rule t
 WHERE t.layer_id=?
 ORDER BY t.name;
@@ -208,7 +210,7 @@ ORDER BY t.name;
 
 opruleexSQL=\
 """
-SELECT t.name,t.definition
+SELECT t.name,t.definition, used
 FROM expression t
 WHERE t.layer_id=?
 ORDER BY t.name;
@@ -216,7 +218,7 @@ ORDER BY t.name;
 
 opruletsSQL=\
 """
-SELECT inp.name,fill.name,input_file,path
+SELECT inp.name,fill.name,input_file,path, used
 FROM input_time_series_oprule inp,fill_in_type_description fill
 WHERE layer_id=? 
 AND inp.fillin = fill.fill_in_type_id
@@ -225,14 +227,15 @@ ORDER BY inp.name;
 
 groupSQL=\
 """
-SELECT name from groups 
+SELECT name, used
+FROM groups 
 WHERE layer_id=?
 ORDER BY name;
 """
 
 paramSQL=\
 """
-SELECT param.name, pval.parameter_value
+SELECT param.name, pval.parameter_value, used
 FROM model_parameter_description param, model_parameter_values pval
 WHERE param.model_parameter_id=pval.model_parameter_id
 AND layer_id = ?
@@ -243,7 +246,7 @@ ORDER BY param.name;
 
 groupmemberSQL=\
 """
-SELECT groups.name,obj.name,identifier
+SELECT groups.name,obj.name,identifier, used
 FROM groups,group_member,object_type_description obj
 WHERE layer_id=?
 AND groups.group_id=group_member.group_id
@@ -253,7 +256,7 @@ ORDER BY groups.name, obj.name, identifier;
 
 ratecoeffSQL=\
 """
-SELECT group_name,const.name,ratvar.name,coefficient_value
+SELECT group_name,const.name,ratvar.name,coefficient_value, used
 FROM rate_coefficient rate,nc_constituent_description const,
 rate_variable_description ratvar
 WHERE rate.rate_variable_id=ratvar.rate_variable_id
