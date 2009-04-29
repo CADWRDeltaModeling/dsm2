b29369108af2dc1b90f344988ffbb39d7f368775
No Author <noauthor@water.ca.gov>
2009-04-29 22:45:56 +0000

    Just started.
    git-svn-id: http://dminfo:8686/svn/repository/models/trunk@804 cdc4813b-4270-ec4f-936e-925f93a782c2

--- dsm2_distribute/dsm2/bin/gui.bat
+++
@@ -1,8 +0,0 @@
-setlocal
-set dsm2guipath=..\lib\dsm2help.jar;..\lib\jh.jar;..\lib\DatabaseGUI.jar;..\lib\jdnc-0_7-all.jar
-set dsm2guipath=%dsm2guipath%;..\lib\dx.jar;..\lib\dbswing.jar;..\lib\beandt.jar;
-set dsm2app=dwr.dms.appframework.Application
-:: the redirection did not work so well with javaw
-start javaw.exe -classpath %dsm2guipath%  -mx100m -ea %dsm2app% dsm2interface.properties 2>err.log
-endlocal
-
--- dsm2_distribute/dsm2/bin/informixtoaccess/informix2access.bat
+++
@@ -1,4 +0,0 @@
-copy .\dbutil\copy_informax_db\template\dsm2input.mdb .\dbutil\copy_informax_db\dist /Y 
-.\dbutil\copy_informax_db\dist\i2m_dbtranslator_ado.exe .\dbutil\copy_informax_db\dist\schema.sql
-copy .\dbutil\copy_informax_db\dist\dsm2input.mdb /Y
-del /F .\dbutil\copy_informax_db\dist\dsm2input.mdb
