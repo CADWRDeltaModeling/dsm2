7463165a755e77ef6ba70b81c37865d9b0bc88b3
No Author <noauthor@water.ca.gov>
2009-06-10 23:07:04 +0000

    Converting to v8 (text-based)
    git-svn-id: http://dminfo:8686/svn/repository/models/trunk/dsm2_distribute@892 cdc4813b-4270-ec4f-936e-925f93a782c2

--- dsm2/tutorials/simulations/historical/createme.bat
+++ dsm2/tutorials/simulations/historical/createme.bat
@@ -2,28 +2,13 @@
 :: strip out all the Informix references and replace them with Access
 echo off
 SETLOCAL
-set base=historic
+set base=historical
 
 SET origloc=%DSM2_HOME%\study_templates\%base%
 echo Copying from %origloc%
 if not exist %origloc% echo Original location %origloc% not found
 xcopy /Y/Q %origloc%\*.* .
 
-echo moving scripts to temporary location
-for %%f in (hydro*.inp) do move %%f %%f.move_to_access
-for %%f in (qual*.inp) do move %%f %%f.move_to_access
-for %%f in (ptm*.inp) do move %%f %%f.move_to_access
-for %%f in (config*.inp) do move %%f %%f.move_to_access
-
-echo removing informix references
-SET MOVE_SCRIPT=%DSM2_HOME%\vista\bin\vscript %DSM2_HOME%\scripts\move_to_access.py
-for %%f in (hydro*.inp.move_to_access) do call %MOVE_SCRIPT% %%f
-for %%f in (qual*.inp.move_to_access) do call %MOVE_SCRIPT% %%f
-for %%f in (ptm*.inp.move_to_access) do call %MOVE_SCRIPT% %%f
-for %%f in (config*.inp.move_to_access) do call %MOVE_SCRIPT% %%f
-
-ENDLOCAL
-del *.*.move_to_access
 if not exist .\output mkdir .\output
 
 
