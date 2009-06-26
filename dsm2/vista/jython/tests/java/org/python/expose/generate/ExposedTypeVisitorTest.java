999c714868374f08184b4bebd3ba4b2111d9a570
No Author <noauthor@water.ca.gov>
2009-06-26 03:38:53 +0000

    jython version 2.5
    git-svn-id: http://dminfo:8686/svn/repository/models/trunk/dsm2_distribute@1008 cdc4813b-4270-ec4f-936e-925f93a782c2

---
+++ dsm2/vista/jython/tests/java/org/python/expose/generate/ExposedTypeVisitorTest.java
@@ -0,0 +1,54 @@
+package org.python.expose.generate;
+
+import junit.framework.TestCase;
+
+import org.objectweb.asm.Type;
+import org.python.core.PyObject;
+import org.python.expose.generate.ExposedTypeVisitor;
+
+public class ExposedTypeVisitorTest extends TestCase {
+
+    public void setUp() {
+        etv = new ExposedTypeVisitor(Type.getType("Lsimpletype;"), null) {
+
+            @Override
+            public void handleResult(String name) {
+                result = name;
+            }
+
+            @Override
+            public void handleResult(Type base) {
+                baseResult = base;
+            }
+
+            @Override
+            public void handleResult(boolean isBaseType) {
+                isBaseTypeResult = isBaseType;
+            }
+        };
+    }
+
+    public void testSimpleType() {
+        etv.visitEnd();
+        assertEquals("simpletype", result);
+        assertEquals(Type.getType(Object.class), baseResult);
+    }
+
+    public void testNamedType() {
+        etv.visit("name", "different");
+        etv.visit("base", Type.getType(PyObject.class));
+        etv.visit("isBaseType", false);
+        etv.visitEnd();
+        assertEquals("different", result);
+        assertEquals(Type.getType(PyObject.class), baseResult);
+        assertEquals(false, isBaseTypeResult);
+    }
+
+    ExposedTypeVisitor etv;
+
+    private String result;
+
+    private Type baseResult;
+
+    private boolean isBaseTypeResult;
+}
