90ac09ecb1785dffe2ff60be373bb5a4b6c6a1d3
No Author <noauthor@water.ca.gov>
2009-07-03 19:03:28 +0000

    git-svn-id: http://dminfo:8686/svn/repository/models/trunk@1087 cdc4813b-4270-ec4f-936e-925f93a782c2
    Former-commit-id: 4d00c54bfdab01cbb98da884dc195e9042992645

--- dsm2_distribute/dsm2/bin/tidy_files.bat
+++
@@ -1,8 +0,0 @@
-echo off
-echo processing
-FOR %%i in (*.inp) do echo %%i
-FOR %%i in (*.inp) do (
-echo Processing %%i
-call vscript.bat %~dp0\..\scripts\dsm2_tidy.py %%i %%i_tmp.inp
-move /y %%i_tmp.inp %%i
-)
