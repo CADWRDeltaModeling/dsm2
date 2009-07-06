d779402f5e5709763ee3467c9611893d740c1110
No Author <noauthor@water.ca.gov>
2009-07-06 19:27:34 +0000

    Added two utility scripts. dsm2_tidy.bat calls dsm2_tidy.py and is for tidying up user files. transfer.bat callse transfer.py and allows the user to transfer information from one file to another, with options for slicing the data in time or performing transformations.
    git-svn-id: http://dminfo:8686/svn/repository/models/trunk@1096 cdc4813b-4270-ec4f-936e-925f93a782c2
    
    Former-commit-id: 628c449245480b25d1a7d89e40c98a6604648212

---
+++ dsm2_distribute/dsm2/bin/transfer.bat
@@ -0,0 +1,10 @@
+
+::Launches the script transfer.py
+::This script allows you to copy time series from one DSS file to another
+::Optionally, you can slice the time series in time
+::Optionally, you can perform a transformation (period ave rio 
+
+call vscript.bat %%~dp0\..\scripts\transfer.py %%1 %%2 %%3 %%4 %%5 %%6 %%7 %%8
+
+
+
