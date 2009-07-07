d89e2fc15ca5bcad97e9cfe2f2f5cfdace9fe907
No Author <noauthor@water.ca.gov>
2009-07-07 16:47:59 +0000

    ptm.bat is modified to respond the change of the command.
    git-svn-id: http://dminfo:8686/svn/repository/models/trunk/dsm2_distribute@1117 cdc4813b-4270-ec4f-936e-925f93a782c2
    
    Former-commit-id: b3e7048546709c51722a51b8077a12cb2c2d310b

--- dsm2/bin/ptm.bat
+++ dsm2/bin/ptm.bat
@@ -1,16 +1,15 @@
 @echo off
 
-
 if exist %dsm2_home%/bin/ptm.jar goto :valid
 
 :notfound
-echo ## An Error has occured in the batch file. PTM_HOME not set correctly or ptm.jar not found.##
+echo ## An Error has occured in the batch file. DSM2_HOME not set correctly or ptm.jar not found.##
 PAUSE
 goto :end
 
 :valid
 
-set path=%dsm2_hom3%/bin;%dsm2_home%/lib;%path%
+set path=%dsm2_home%/bin;%path%
 
 java -ss1m -mx128m -oss1m -classpath "%dsm2_home%/bin/ptm.jar;%dsm2_home%/bin/COM.jar;%dsm2_home%/bin/edu.jar;%dsm2_home%/bin/xml.jar" DWR.DMS.PTM.MainPTM %1
 
