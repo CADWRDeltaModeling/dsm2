ba7d9add7b4c91b3d39155882b0d1b4872035bf3
No Author <noauthor@water.ca.gov>
2009-07-13 21:12:41 +0000

    Modified license header.
    git-svn-id: http://dminfo:8686/svn/repository/models/trunk@1161 cdc4813b-4270-ec4f-936e-925f93a782c2
    
    Former-commit-id: f5169ee7c243c5e57ee4dc91dd11ec5b5652d815

--- dsm2/src/fixed/dsm2_init.f
+++ dsm2/src/fixed/dsm2_init.f
@@ -1,20 +1,21 @@
 C!<license>
-C!    Copyright (C) 1996, 1997, 1998, 2001, 2007 State of California,
+C!    Copyright (C) 1996, 1997, 1998, 2001, 2007, 2009 State of California,
 C!    Department of Water Resources.
 C!    This file is part of DSM2.
 
-C!    DSM2 is free software: you can redistribute it and/or modify
-C!    it under the terms of the GNU General Public !<license as published by
-C!    the Free Software Foundation, either version 3 of the !<license, or
+C!    The Delta Simulation Model 2 (DSM2) is free software: 
+C!    you can redistribute it and/or modify
+C!    it under the terms of the GNU General Public License as published by
+C!    the Free Software Foundation, either version 3 of the License, or
 C!    (at your option) any later version.
 
 C!    DSM2 is distributed in the hope that it will be useful,
 C!    but WITHOUT ANY WARRANTY; without even the implied warranty of
 C!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-C!    GNU General Public !<license for more details.
+C!    GNU General Public License for more details.
 
-C!    You should have received a copy of the GNU General Public !<license
-C!    along with DSM2.  If not, see <http://www.gnu.org/!<licenses/>.
+C!    You should have received a copy of the GNU General Public License
+C!    along with DSM2.  If not, see <http://www.gnu.org/licenses>.
 C!</license>
 
       subroutine dsm2_init
