cbf6b37b328e107c86dc364e8a5a0eb96b4c2e53
No Author <noauthor@water.ca.gov>
2009-07-13 17:13:02 +0000

    changed to relative path.
    git-svn-id: http://dminfo:8686/svn/repository/models/trunk/dsm2@1157 cdc4813b-4270-ec4f-936e-925f93a782c2

--- src/common/version_generate.py
+++ src/common/version_generate.py
@@ -1,7 +1,7 @@
 import os
 
 
-VersionTemplate     = "      character*48 :: dsm2_version = '8.0a4  Subversion: @Version_SVN@' " 
+VersionTemplate     = "      character*48 :: dsm2_version = '8.0a4  Subversion: @{Version_SVN}' " 
 VersionFile_path    = "./version.inc"
 
 
@@ -11,12 +11,12 @@ VersionFile_path    = "./version.inc"
 VersionFile = open(VersionFile_path, "w") 
 
 try:
-    (dummy, SVNVersion_SourceCode) = os.popen4("svnversion D:/delta/models/dsm2 ")
+    (dummy, SVNVersion_SourceCode) = os.popen4("svnversion ../../../dsm2 ")
     SVNVersion_SourceCode = SVNVersion_SourceCode.readlines()[0]
     SVNVersion_SourceCode = SVNVersion_SourceCode.strip()
 
-    print ' SVN version of D:/delta/models/dsm2:        '+ SVNVersion_SourceCode
-    VersionTxt = VersionTemplate.replace("@Version_SVN@", SVNVersion_SourceCode)
+    print ' SVN version of ../../../dsm2:        '+ SVNVersion_SourceCode
+    VersionTxt = VersionTemplate.replace("@{Version_SVN}", SVNVersion_SourceCode)
     VersionFile.write(VersionTxt)
     VersionFile.close()
    
@@ -24,4 +24,4 @@ try:
 except:
     VersionFile.close()
     os.remove(VersionFile_path) 
-    print 'Abort.... possible error in file D:/delta/models/dsm2/src/common/verion_generate.py'    
+    print 'Abort.... possible error in file /delta/models/dsm2/src/common/verion_generate.py'    
