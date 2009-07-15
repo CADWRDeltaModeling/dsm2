d75bb03da759b0b8878bbf274eef5020ed31ffff
No Author <noauthor@water.ca.gov>
2009-07-15 17:30:27 +0000

    Minor fix and modification of file headers in PTM. KJ
    git-svn-id: http://dminfo:8686/svn/repository/models/trunk/dsm2@1207 cdc4813b-4270-ec4f-936e-925f93a782c2

--- src/ptm/DWR/DMS/PTM/Conveyor.java
+++ src/ptm/DWR/DMS/PTM/Conveyor.java
@@ -1,4 +1,4 @@
-//    Copyright (C) 1996 State of California, Department of Water
+//    Copyright (C) 1996, 2009 State of California, Department of Water
 //    Resources.
 //
 //    Delta Simulation Model 2 (DSM2): A River, Estuary, and Land
