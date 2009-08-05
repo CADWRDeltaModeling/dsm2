c4c5c05dde8237b178e80f0969cbdf5f622d1bff
Kevin Kao <kkao@water.ca.gov>
2009-08-05 20:16:30 +0000

    diffusion_version_12.f90
    git-svn-id: http://dminfo:8686/svn/repository/models/trunk@1252 cdc4813b-4270-ec4f-936e-925f93a782c2
    
    Former-commit-id: f301b89ff021f68bb63d7729f211cd00e475a741

--- dsm2_stm/src/diffusion.f90
+++ dsm2_stm/src/diffusion.f90
@@ -28,44 +28,78 @@
 !
 ! It solves the dispersion of sediment in the longitudinal direction
 ! Partial_(A C_s)/Partial_t = Partial/Partial_x [A K_s Partial_(C_s)/Partial_x]
+! 
+! These sub-routines are being developed by the Department of Civil and 
+! Environmental Engineering at the University of California, Davis and DWR
 !
 ! Record of revisions:
-!     Date               Programmers                 Description of change
-!     ====               ===========                 =====================
+!       Date             Programmers                 Description of change
+!       ====             ===========                 =====================
 !     07/21/09          Kaveh Zamani               Version 0 - Basic structure
 !                 and Fabian Bombardelli                of the code
+!
+!     07/22/09          Kaveh Zamani               More elements of the code
+!                 and Fabian Bombardelli
+!
+!     07/26/09        Fabian Bombardelli        Computation of values of matrix
+!     07/27/09        Fabian Bombardelli        Boundary conditions and solutions  
+!     07/28/09        Fabian Bombardelli        Case 1 and final set up
+!     07/29/09        Fabian Bombardelli        Case 2 and final set up
+!     07/30/09        Fabian Bombardelli        Optimizing Case 2
 !****************************************************************************
 
-     implicit none
+          IMPLICIT NONE
 
 ! Variables: data dictionary.  Define variables types, definitions and units
 
-! C_s: concentration of sediment in suspension averaged in the cross section
-! K_s(i): dispersion coefficient at the face of volume i
-! Area(i): Area of cross section evaluated at the center of volume i
-!          This is an output coming from HYDRO
+! C_s_n_plus_one(i): concentration of sediment in suspension averaged in the volume i
+!      for time t + 1
+! C_S_n(i): concentration of sediment in suspension averaged in the volume
+!         for time t
+! K_s_n_plus_one(i): dispersion coefficient at the left face of volume i for time t + 1
+! K_s_n(i): dispersion coefficient at the left face of volume i for time t
+! Area_n_plus_one(i): Area of cross section evaluated at the left face of volume i at time
+!                     t + 1. This is an output coming from HYDRO
+! Area_n(i): Area of cross section evaluated at the left face of volume i at time
+!            t. This is an output coming from HYDRO
+! A(i): Values of the coefficients below diagonal in matrix
+! B(i): Values of the coefficients at the diagonal in matrix
+! C(i): Values of the coefficients above diagonal in matrix
+! D(i): Values of the independent term
 ! N_volumes: Number of volumes (computational cross sections) whose
 !          sediment concentration will be computed
 ! dt_sed: Time step for the computation of sediment transport (in seconds)
-! dx_sed: Spatial step for the computation of sediment transport (in feet)  
-      
-       INTEGER :: N_volumes = 100
-       REAL, DIMENSION(N_volumes) :: Area
-       REAL, DIMENSION(N_volumes+1) :: K_s
-       REAL, DIMENSION(N_volumes) :: C_s
- 
- ! Theta is included with a value for the time being 
-       REAL :: Theta = 0.5 
-       REAL :: dt_sed
-       REAL :: dx_sed
-       
+! dx_sed: Spatial step for the computation of sediment transport (in feet for STM)  
+
+       INTEGER, PARAMETER :: N_volumes = 21
+       INTEGER :: N
+       INTEGER :: i, j, N_times, ak
+       INTEGER :: i_case
        INTEGER :: Boundary_condition_flag_upstream
        INTEGER :: Boundary_condition_flag_downstream
-
-!CCCCCCCCCCCCCC  Temporary statements while the code is not connected to Hydro CCC Begining
+       
+       REAL, DIMENSION(N_volumes+1) :: Area_n, Area_n_plus_one
+       REAL, DIMENSION(N_volumes+1) :: K_s_n, K_s_n_plus_one
+       REAL, DIMENSION(N_volumes) :: C_s_n, C_s_n_plus_one
+       REAL, DIMENSION(N_volumes) :: A, B, C, D, X
+       
+       REAL, PARAMETER :: Pi = 3.1415926535897932384626433832795
+       
+       REAL :: dt_sed
+       REAL :: dx_sed
+       REAL :: Theta
+       REAL :: Cal_D, Cal_D_one
+       
+       REAL :: Time
+       REAL :: C_s_upstream, C_s_downstream
+       
+       REAL :: ax
+       REAL :: Flux_s_upstream_n, Flux_s_upstream_n_plus_one
+                    
+!CCCCCCCCCCCCCC  Temporary statements while the code is not connected to Hydro CCC Beginning
 ! We assume a value of N_volumes and other variables for the time being   
-       dt_sed = 10
-       dx_sed = 20
+         Theta = 0.5
+         dt_sed = 0.001
 !CCCCCCCCCCCCCC  Temporary statements while the code is not connected to Hydro CCC End
 
 ! Place holder for input of data from HYDRO
@@ -73,72 +107,267 @@
 ! get_Ks_Hydro
 ! We need to discuss how to compute K_s from the output of HYDRO
 
-! BODY OF DIFFUSION
+!CCCC
+! BODY OF SUB-ROUTINE DIFFUSION
+          i_case = 2
 
-! Computation of matrix for internal nodes 
+          if(i_case.eq.1)dx_sed = 0.05
+          if(i_case.eq.2)dx_sed = 0.045
+     
+          N = N_volumes
+          Cal_D = Theta * dt_sed / dx_sed /dx_sed
+          Cal_D_one = (1 - Theta) * dt_sed / dx_sed / dx_sed
 
-! Assembling the matrix
+!CCCCCCCCCCCCCC  Temporary statements while the code is not connected to Hydro CCC Beginning
+        open(3,file='results.dat',status='unknown')
+
+        do i=1,N+1
+           Area_n(i) = 1.0d0
+           Area_n_plus_one(i) = 1.0d0
+           K_s_n(i) = 1.0d0
+           K_s_n_plus_one(i) = 1.0d0
+        end do
+
+! Initial condition
+     if(i_case.eq.1) then
+       do ak=1, (N - 1) / 2 + 1
+         C_s_n(ak) = 1.0d0/0.5d0 * (ak - 1) * dx_sed
+       end do
+              
+       do ak=(N - 1) / 2 + 2, N
+         C_s_n(ak) = 2.0d0 - 1.0d0/0.5d0 * (ak - 1) * dx_sed
+       end do
+       C_s_n(N)=0.0d0
+     end if
+     
+     if(i_case.eq.2)then
+       do ak=1, N
+         ax = (ak - 1) * dx_sed + 0.1
+         C_s_n(ak) = 2.0d0 * ax + 4.0d0 * cos(0.5d0 * Pi * ax)
+!         print *, C_s_n(ak)
+       end do
+     end if
+
+!     do ak=1, N
+!       print *, C_s_n(ak)
+!     end do
+
+! A time-advancing stage is started here to validate the code
+
+        N_times = 500
+
+       do j=1,N_times
+
+         Time = dt_sed * j
+         
+         write(3,*)j,dt_sed,Time
+         write(3,*)
+
+
+!CCCCCCCCCCCCCC  Temporary statements while the code is not connected to Hydro CCC End
+
+! Initialization of values of the matrix
+         do i=1,N
+           A(i)=0.0d0
+           B(i)=0.0d0
+           C(i)=0.0d0
+           D(i)=0.0d0
+           X(i)=0.0d0
+         end do
+
+! Computation of all elements of matrix 
+       call values(A, B, C, D, N, Cal_D, Cal_D_one, Area_n, Area_n_plus_one, K_s_n, K_s_n_plus_one, C_s_n)
+
+        do i=1,N
+          print *, i, A(i), B(i), C(i), D(i)
+        end do
+!          print *, Cal_D, Cal_D_one
+
+! Imposing the boundary conditions
+
+       if(i_case.eq.1)then
+           Boundary_condition_flag_upstream = 1
+           Boundary_condition_flag_downstream = 1
+           C_s_upstream = 0.0d0
+           C_s_downstream = 0.0d0
+       end if
+
+       if(i_case.eq.2)then
+           Boundary_condition_flag_upstream = 0
+           Boundary_condition_flag_downstream = 1
+           Flux_s_upstream_n = 2.0d0 - 2.0d0 * Pi * sin(0.05d0 * Pi) * exp(-1.0d0 * Pi * Pi / 4.0d0 * (Time - dt_sed))
+           Flux_s_upstream_n_plus_one = 2.0d0 - 2.0d0 * Pi * sin(0.05d0 * Pi) * exp(-1.0d0 * Pi * Pi / 4.0d0 * Time)
+           
+           write(*,*)
+           print *, Time, Flux_s_upstream_n, Flux_s_upstream_n_plus_one
+           
+           C_s_downstream = 2.0d0
+       end if
+
+       call boundary_conditions(Boundary_condition_flag_upstream, Boundary_condition_flag_downstream, A, B, C, D, N, C_s_upstream, C_s_downstream, Cal_D, Cal_D_one, Area_n, K_s_n, Area_n_plus_one, K_s_n_plus_one, Flux_s_upstream_n, Flux_s_upstream_n_plus_one, dx_sed, C_s_n)
+
+        do i=1,N
+          print *, i, A(i), B(i), C(i), D(i)
+        end do
+          print *, Cal_D, Cal_D_one
 
 ! Call for the tri-diagonal solver
         call TRIDI_SOLVER(A, B, C, D, N, X)
 ! Output the results
+        do i=1, N
+         write(3,*)X(i)
+         C_s_n(i)=X(i)
+        end do
+        write(3,*)
+
+        end do
 
         end program diffusion
 
+!*****************************************************************************************
+        SUBROUTINE values(A, B, C, D, N, Cal_D, Cal_D_one, Area_n, Area_n_plus_one, K_s_n, K_s_n_plus_one, C_s_n)
+
+! Variables: data dictionary.  Define variables types, definitions and units
+
+! Area_aux_n: Area evaluated at the center of the volume i at time t
+! Areas_aux_n_plus_one: Area evaluated at the center of the volume i at time t + 1
+! A(i): Values of the coefficients below diagonal in matrix
+! B(i): Values of the coefficients at the diagonal in matrix
+! C(i): Values of the coefficients above diagonal in matrix
+! D(i): Values of the independent term
+
+          INTEGER, PARAMETER :: N_volumes = 21
+          INTEGER N
+          INTEGER i, j, N_times, ak
+          REAL, DIMENSION(N_volumes) :: A, B, C, D, X
+          REAL, DIMENSION(N_volumes+1) :: Area_n, Area_n_plus_one
+          REAL, DIMENSION(N_volumes+1) :: K_s_n, K_s_n_plus_one
+          REAL, DIMENSION(N_volumes) :: C_s_n, C_s_n_plus_one
+          REAL :: Area_aux_n, Area_aux_n_plus_one
+          
+          do ak=1, N
+            print *, C_s_n(ak)
+          end do
+          
+           do i=1,N
+            Area_aux_n_plus_one = (Area_n_plus_one(i) + Area_n_plus_one(i+1)) / 2.0d0
+            Area_aux_n = (Area_n(i) + Area_n(i+1)) / 2.0d0
+            B(i)=Area_aux_n_plus_one + Cal_D * Area_n_plus_one(i+1) * K_s_n_plus_one(i+1) 
+            B(i)=B(i) + Cal_D * Area_n_plus_one(i) * K_s_n_plus_one(i) 
+            A(i)= - 1.0d0 * Cal_D * Area_n_plus_one(i) * K_s_n_plus_one(i)
+            C(i)= - 1.0d0 * Cal_D * Area_n_plus_one(i+1) * K_s_n_plus_one(i+1)
+            D(i)=Area_aux_n * C_s_n(i) + Cal_D_one * C_s_n(i+1) * Area_n(i+1) * K_s_n(i+1)
+            D(i)= D(i) -  Cal_D_one * C_s_n(i) * Area_n(i+1) * K_s_n(i+1)
+            D(i)= D(i) -  Cal_D_one * C_s_n(i) * Area_n(i) * K_s_n(i)
+            D(i)= D(i) +  Cal_D_one * C_s_n(i-1) * Area_n(i) * K_s_n(i)
+           end do
+                 
+        RETURN
+        END
+        
+
+!*****************************************************************************************    
+        SUBROUTINE boundary_conditions(Boundary_condition_flag_upstream, Boundary_condition_flag_downstream, A, B, C, D, N, C_s_upstream, C_s_downstream, Cal_D, Cal_D_one, Area_n, K_s_n, Area_n_plus_one, K_s_n_plus_one, Flux_s_upstream_n, Flux_s_upstream_n_plus_one, dx_sed, C_s_n)
+        
+           INTEGER, PARAMETER :: N_volumes = 21
+           INTEGER :: Boundary_condition_flag_upstream
+           INTEGER :: Boundary_condition_flag_downstream
+           REAL, DIMENSION(N_volumes) :: A, B, C, D, X
+           REAL, DIMENSION(N_volumes+1) :: Area_n, Area_n_plus_one
+           REAL, DIMENSION(N_volumes+1) :: K_s_n, K_s_n_plus_one
+           REAL :: C_s_upstream, C_s_downstream
+           REAL :: Flux_s_upstream_n, Flux_s_upstream_n_plus_one
+           REAL :: dx_sed
+           REAL :: Cal_D, Cal_D_one
+           REAL, DIMENSION(N_volumes) :: C_s_n, C_s_n_plus_one
+           
+           print *, Flux_s_upstream_n, Flux_s_upstream_n_plus_one, dx_sed
+           print *, Cal_D, Cal_D_one
+           print *, Area_n_plus_one(1), Area_n(1)
+           print *, C(1)
+
+           If(Boundary_condition_flag_upstream.eq.1)then
+             B(1) = 1.0d0
+             C(1) = 0.0d0
+             D(N) = C_s_upstream
+           end if
+        
+           If(Boundary_condition_flag_upstream.eq.0)then
+             B(1) = B(1)
+             C(1) = C(1) - Cal_D * Area_n_plus_one(1) * K_s_n_plus_one(1)
+             D(1) = D(1) - 2.0d0 * dx_sed * Flux_s_upstream_n_plus_one * Cal_D * Area_n_plus_one(1) * K_s_n_plus_one(1)
+             D(1) = D(1) - 2.0d0 * dx_sed * Flux_s_upstream_n * Cal_D_one * Area_n(1) * K_s_n(1)
+             D(1) = D(1) + Cal_D * Area_n(1) * K_s_n(1) * C_s_n(2) 
+           end if
+           
+           If(Boundary_condition_flag_downstream.eq.1)then
+             B(N)=1.0d0
+             A(N)= 0.0d0
+             D(N) = C_s_downstream
+           end if
+        
+           If(Boundary_condition_flag_downstream.eq.0)then
+                
+           end if
+           
+
+        RETURN
+        END
 
 !*****************************************************************************************    
         SUBROUTINE TRIDI_SOLVER(A, B, C, D, N, X)
     
-!    I (A,B,C,D,N,
-!    O     X)
-!+++  PURPOSE +++
-! SOLVE OF A TRIDIAGONAL MATRIX FOR X
-!             [A1,C2,00,00,00]  
-! [X1,X2,...] [B1,A2,C3,00,00] = [D1,D2,....]
-!             [00,00,B3,A4,C5]  
-! +++ ARGUMENTS+++
+!    Input (A,B,C,D,N,
+!    Output     X)
+!
+! PURPOSE:
+
+! Solves a tridiagonal system for X. This sub-routine was taken from the Numerical Recipes,
+! page 43 (Edition of 1992)
+! 
+!             [B1,C1,00,00,00]  
+! [X1,X2,...] [A2,B2,C2,00,00] = [D1,D2,....]
+!             [00,A3,B3,C3,00]  
+!             [00,00,A4,B4,C4]
+!             [00,00,00,A5,B5]    
+
+! Variables:
+!
+! A(i): Values of the coefficients below diagonal in matrix
+! B(i): Values of the coefficients at the diagonal in matrix
+! C(i): Values of the coefficients above diagonal in matrix
+! D(i): Values of the independent term 
+! X(i): Values of the computed solution
 
         INTEGER :: N
-        REAL :: A(10000),B(10000),C(10000),D(10000),X(10000)
-
-!+++ ARGUMENT DEFINITIONS +++
-! A: DIAGONAL COEFFICIENTS
-! B: COEFFICIENT BELOW DIAGONAL
-! C: COEFFICIENT ABOVE DIAGONAL
-! D: CONSTANT TERM 
-! X: UNKNOWN OR SOLUTION 
-
-!+++ LOCAL VARIABLES ++++
-        INTEGER :: K,KK
-        REAL :: P(10000),R(10000),T(10000),V(10000)
-!
-! SOLVE
-!
-        P(1)=A(1)
-        FIRST_3_DIA: DO K=1,N-1
-           R(K)=B(K) 
-        FIRST_3_DIA CONTINUE 
-!
-        SECOUND_3_DIA: DO K=2,N
-          T(K)=C(K)/P(K-1)
-          P(K)=A(K)-(T(K)*V(K-1))
-        SECOUND_3_DIA CONTINUE
-!
-        V(1)=D(1)
-        THIRD_3_DIA: DO K=2,N
-          V(K)=D(K)-(T(K)*V(K-1))
-        THIRD_3_DIA CONTINUE
-!
-        X(N)=V(N)/P(N)
-        FORTH_3_DIA DO KK=1,N-1
-          K=N-KK
-          X(K)=(V(K)-R(K)*X(K+1)))/P(K)
-        FORTH_3_DIA CONTINUE
-!
-RETURN
-END 
-  
-
+        REAL, DIMENSION(N) :: A, B, C, D, X
+        INTEGER :: K, KK
+        REAL, DIMENSION(N) :: gam
+        REAL :: bet
+        
+        if(B(1).eq.0)then
+          print *, 'There is a problem here'
+          stop
+        end if
+        
+        bet = B(1)
+        X(1) = D(1) / bet
+        
+        do K=2, N
+          gam(K) = C(K - 1) / bet
+          bet = B(K) - A(K) * gam(K)
+          if(bet.eq.0)then
+            print *, 'Tridiagonal solver failed'
+          end if
+          X(K) = (D(K) - A(K) * X(K - 1)) / bet 
+        end do
+      
+        do KK= N-1, 1, -1
+          X(KK) = X(KK) - gam(KK + 1) * X(KK + 1)
+        end do
+      
+       RETURN
+       END 
 !*****************************************************************************************
     
     
