10b9f68f6cc4cc54a102f83d1c25ab0db014c66c
Kijin Nam <knam@water.ca.gov>
2009-10-22 16:57:39 +0000

    Changed the size of max mem of the Java virtual machine to 512Mb in the PTM batch file.  Hope this prevent some crashes.
    git-svn-id: http://dminfo:8686/svn/repository/models/branches/dsm2_distribute_v8_0@1702 cdc4813b-4270-ec4f-936e-925f93a782c2
    
    Former-commit-id: 9b1450786aac0d1615bff8526b31158e4840804b

--- dsm2/bin/ptm.bat
+++ dsm2/bin/ptm.bat
@@ -9,7 +9,7 @@ goto :end
 
 :valid
 
-java -ss1m -mx128m -oss1m -classpath "%dsm2_home%/bin/ptm.jar;%dsm2_home%/bin/COM.jar;%dsm2_home%/bin/edu.jar;%dsm2_home%/bin/xml.jar" DWR.DMS.PTM.MainPTM %1
+java -ss1m -mx512m -oss1m -classpath "%dsm2_home%/bin/ptm.jar;%dsm2_home%/bin/COM.jar;%dsm2_home%/bin/edu.jar;%dsm2_home%/bin/xml.jar" DWR.DMS.PTM.MainPTM %1
 
 :end
 
