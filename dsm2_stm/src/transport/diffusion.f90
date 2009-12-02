fb1b808e4d372a5b11f88857f918d95b9ea0faa8
Kevin Kao <kkao@water.ca.gov>
2009-12-02 21:47:02 +0000

    added description for diffusion API.
    git-svn-id: http://dminfo:8686/svn/repository/models/trunk@1767 cdc4813b-4270-ec4f-936e-925f93a782c2
    
    Former-commit-id: dcbda9633e00ea7ac8023b016333fe362aa793fe

--- dsm2_stm/src/transport/diffusion.f90
+++ dsm2_stm/src/transport/diffusion.f90
@@ -3,4 +3,45 @@
 !>@ingroup transport
 module diffusion
 
+! We need a single step routine with inputs and outputs that
+! do not involve computation detail (like matrices)
+! This is just stolen from the interface for advection, 
+! but the real diffusion API will look similar
+!subroutine diffuse(mass,     &
+!                  mass_prev,&
+!                  flow,     &                  
+!                  flow_lo,  &
+!                  flow_hi,  &
+!                  area,     &
+!                  area_prev,&
+!                  area_lo,  &
+!                  area_hi,  &
+!                  ncell,    &
+!                  nvar,     &
+!                  time,     &
+!                  dt,       &
+!                  dx)
+
+! This routine should give the effects of diffusion fluxes on each cell
+! for a single time step (ie, explicit). This is needed for the advection step.
+! It is also probably part of the right hand side of the implicit diffusion solver 
+! matrix calculation. 
+!subroutine diffusion_operator(mass,     &
+!                  mass_prev,&
+!                  flow,     &                  
+!                  flow_lo,  &
+!                  flow_hi,  &
+!                  area,     &
+!                  area_prev,&
+!                  area_lo,  &
+!                  area_hi,  &
+!                  ncell,    &
+!                  nvar,     &
+!                  time,     &
+!                  dt,       &
+!                  dx)
+
+! The rest of this should be neat, but is not important to the public use of the library
+
+
 end module
