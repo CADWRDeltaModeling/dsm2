2b5cd056bdf4e0022e276607a29509cf21f9138b
Kevin Kao <kkao@water.ca.gov>
2010-03-18 22:06:43 +0000

    ignore userDefineLang.xml and change binary copy path in copy_hydro_qual_ptmDLL_notepad_style.bat
    git-svn-id: http://dminfo:8686/svn/repository/models/branches/dsm2_distribute_v8_0@1866 cdc4813b-4270-ec4f-936e-925f93a782c2
    
    Former-commit-id: ddd1972aeb5e5e9214e3be03183703ca629475b4

--- dsm2/install/copy_hydro_qual_ptmDLL_notepad_style.bat
+++ dsm2/install/copy_hydro_qual_ptmDLL_notepad_style.bat
@@ -2,14 +2,14 @@ set version=v8_0_3
 
 echo version "%version%" 
 
-copy ..\..\..\..\branch\dsm2\build_vs2008sp1_ivf110066\qual\Release\qual.exe   ..\..\..\..\branch\dsm2_distribute\dsm2\bin\* 
-copy ..\..\..\..\branch\dsm2\build_vs2008sp1_ivf110066\hydro\Release\hydro.exe ..\..\..\..\branch\dsm2_distribute\dsm2\bin\*
-copy ..\..\..\..\branch\dsm2\build_vs2008sp1_ivf110066\all\DLL\ptm.dll         ..\..\..\..\branch\dsm2_distribute\dsm2\bin\* 
+copy ..\..\..\..\models\dsm2\build_vs2008sp1_ivf110066\qual\Release\qual.exe   ..\..\..\..\models\dsm2_distribute\dsm2\bin\* 
+copy ..\..\..\..\models\dsm2\build_vs2008sp1_ivf110066\hydro\Release\hydro.exe ..\..\..\..\models\dsm2_distribute\dsm2\bin\*
+copy ..\..\..\..\models\dsm2\build_vs2008sp1_ivf110066\all\DLL\ptm.dll         ..\..\..\..\models\dsm2_distribute\dsm2\bin\* 
 
-copy ..\..\..\..\branch\dsm2\build_vs2008sp1_ivf110066\qual\Release\qual.exe   ..\..\..\..\branch\dsm2_distribute\dsm2\bin\qual_%version%.exe
-copy ..\..\..\..\branch\dsm2\build_vs2008sp1_ivf110066\hydro\Release\hydro.exe ..\..\..\..\branch\dsm2_distribute\dsm2\bin\hydro_%version%.exe
-copy ..\..\..\..\branch\dsm2\build_vs2008sp1_ivf110066\all\DLL\ptm.dll         ..\..\..\..\branch\dsm2_distribute\dsm2\bin\ptm_%version%.dll 
-copy ..\..\..\..\branch\dsm2_distribute\dsm2\bin\ptm.jar             ..\..\..\..\branch\dsm2_distribute\dsm2\bin\ptm_%version%.jar
+copy ..\..\..\..\models\dsm2\build_vs2008sp1_ivf110066\qual\Release\qual.exe   ..\..\..\..\models\dsm2_distribute\dsm2\bin\qual_%version%.exe
+copy ..\..\..\..\models\dsm2\build_vs2008sp1_ivf110066\hydro\Release\hydro.exe ..\..\..\..\models\dsm2_distribute\dsm2\bin\hydro_%version%.exe
+copy ..\..\..\..\models\dsm2\build_vs2008sp1_ivf110066\all\DLL\ptm.dll         ..\..\..\..\models\dsm2_distribute\dsm2\bin\ptm_%version%.dll 
+copy ..\..\..\..\models\dsm2_distribute\dsm2\bin\ptm.jar             ..\..\..\..\models\dsm2_distribute\dsm2\bin\ptm_%version%.jar
 
 
 copy /y ..\..\..\dsm2\src\input_storage\component.py .
