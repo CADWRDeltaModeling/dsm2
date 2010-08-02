99f54a5285ca87190470c616a9cde43ad0335ba4
Zhou Yu <yzhou@water.ca.gov>
2010-08-02 15:47:55 +0000

    path changed for version 8.0.5
    git-svn-id: http://dminfo:8686/svn/repository/models/branches/dsm2_distribute_v8_0@1948 cdc4813b-4270-ec4f-936e-925f93a782c2
    
    Former-commit-id: 1a31c0f880bff3cb0ae956933102dae76a9f6b4c

--- dsm2/install/copy_hydro_qual_ptmDLL_notepad_style.bat
+++ dsm2/install/copy_hydro_qual_ptmDLL_notepad_style.bat
@@ -2,18 +2,18 @@ set version=v8_0_5
 
 echo version "%version%" 
 
-copy ..\..\..\..\models\dsm2\build_vs2008sp1_ivf110066\qual\Release\qual.exe   ..\..\..\..\models\dsm2_distribute\dsm2\bin\* 
-copy ..\..\..\..\models\dsm2\build_vs2008sp1_ivf110066\hydro\Release\hydro.exe ..\..\..\..\models\dsm2_distribute\dsm2\bin\*
-copy ..\..\..\..\models\dsm2\build_vs2008sp1_ivf110066\all\DLL\ptm.dll         ..\..\..\..\models\dsm2_distribute\dsm2\bin\* 
+copy ..\..\..\..\models\dsm2_v8_0\build_vs2008sp1_ivf11\all\Release\qual.exe   ..\..\..\..\models\dsm2_distribute\dsm2\bin\* 
+copy ..\..\..\..\models\dsm2_v8_0\build_vs2008sp1_ivf11\all\Release\hydro.exe ..\..\..\..\models\dsm2_distribute\dsm2\bin\*
+copy ..\..\..\..\models\dsm2_v8_0\build_vs2008sp1_ivf11\all\DLL\ptm.dll         ..\..\..\..\models\dsm2_distribute\dsm2\bin\* 
 
-copy ..\..\..\..\models\dsm2\build_vs2008sp1_ivf110066\qual\Release\qual.exe   ..\..\..\..\models\dsm2_distribute\dsm2\bin\qual_%version%.exe
-copy ..\..\..\..\models\dsm2\build_vs2008sp1_ivf110066\hydro\Release\hydro.exe ..\..\..\..\models\dsm2_distribute\dsm2\bin\hydro_%version%.exe
-copy ..\..\..\..\models\dsm2\build_vs2008sp1_ivf110066\all\DLL\ptm.dll         ..\..\..\..\models\dsm2_distribute\dsm2\bin\ptm_%version%.dll 
+copy ..\..\..\..\models\dsm2_v8_0\build_vs2008sp1_ivf11\all\Release\qual.exe   ..\..\..\..\models\dsm2_distribute\dsm2\bin\qual_%version%.exe
+copy ..\..\..\..\models\dsm2_v8_0\build_vs2008sp1_ivf11\all\Release\hydro.exe ..\..\..\..\models\dsm2_distribute\dsm2\bin\hydro_%version%.exe
+copy ..\..\..\..\models\dsm2_v8_0\build_vs2008sp1_ivf11\all\DLL\ptm.dll         ..\..\..\..\models\dsm2_distribute\dsm2\bin\ptm_%version%.dll 
 copy ..\..\..\..\models\dsm2_distribute\dsm2\bin\ptm.jar             ..\..\..\..\models\dsm2_distribute\dsm2\bin\ptm_%version%.jar
 
 
-copy /y ..\..\..\dsm2\src\input_storage\component.py .
-copy /y ..\..\..\dsm2\src\input_storage\userDefineLangTemplate.xml .
+copy /y ..\..\..\dsm2_v8_0\src\input_storage\component.py .
+copy /y ..\..\..\dsm2_v8_0\src\input_storage\userDefineLangTemplate.xml .
 
 start /wait python component.py notepad
 
