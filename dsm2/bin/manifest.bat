0847796cc89cce9a4f4920d05e441adc85965cba
Zhou Yu <yzhou@water.ca.gov>
2013-05-02 03:23:43 +0000

    dsm2 v8.1.1 bin commit
    git-svn-id: http://dminfo:8686/svn/repository/models/trunk/dsm2_distribute@2215 cdc4813b-4270-ec4f-936e-925f93a782c2
    
    Former-commit-id: ffeb58caec9b003b8be671b961ef03565067dfe4

--- dsm2/bin/manifest.bat
+++ dsm2/bin/manifest.bat
@@ -11,14 +11,8 @@ call vscript.bat %%~dp0\..\scripts\manifest.py %1 %2 %3
 goto end
 
 :usage
-echo.
-echo Create a manifest of all inp files from an echoed output file
-echo.
-echo Usage: manifest echofile manifestfile [copydir]
-echo. 
-echo If copydir is given and is empty/nonexistent, 
-echo all files in the manifest will be copied to the directory
-echo.
+echo Usage: manifest echofile manifestfile [dir]
+echo If copydir is given and is empty/nonexistent, all files in the manifest will be copied to the directory
 
 :end
 
