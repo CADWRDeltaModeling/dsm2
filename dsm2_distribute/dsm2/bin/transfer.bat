2969dcdb08dd3895933a9fc0022827cf82898ba8
Zhou Yu <yzhou@water.ca.gov>
2013-05-02 03:23:43 +0000

    dsm2 v8.1.1 bin commit
    git-svn-id: http://dminfo:8686/svn/repository/models/trunk@2215 cdc4813b-4270-ec4f-936e-925f93a782c2
    
    Former-commit-id: 9c5f9782d23abf60dff90e6b33bb60f6728fea60

--- dsm2_distribute/dsm2/bin/transfer.bat
+++ dsm2_distribute/dsm2/bin/transfer.bat
@@ -3,13 +3,8 @@
 ::This script allows you to copy time series from one DSS file to another
 ::Optionally, you can slice the time series in time
 ::Optionally, you can perform a transformation (period ave rio 
-SET bindir=%~dp0
-SET p1=%1
-SET p2=%2
-SET p3=%3
-SHIFT
-SHIFT
-SHIFT
-call vscript.bat %bindir%\..\scripts\transfer.py %p1% %p2% %p3% %%1 %%2 %%3 %%4 %%5 %%6 %%7 %%8 %%9
+
+call vscript.bat %%~dp0\..\scripts\transfer.py %%1 %%2 %%3 %%4 %%5 %%6 %%7 %%8 %%9
+
 
 
