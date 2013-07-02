146c33ddaf1492282f788b2ef2578eebe910c676
Xiaochun Wang <xwang@water.ca.gov>
2013-07-02 22:56:41 +0000

    new class for adding behaviors
    git-svn-id: http://dminfo:8686/svn/repository/models/branches/ptm_development@2233 cdc4813b-4270-ec4f-936e-925f93a782c2

---
+++ src/ptm/DWR/DMS/PTM/NonPhysicalBarrier.java
@@ -0,0 +1,81 @@
+package DWR.DMS.PTM;
+import java.util.Map;
+import java.util.Iterator;
+import java.util.Calendar;
+
+public class NonPhysicalBarrier {
+	// ids are internal env indices
+	public NonPhysicalBarrier(int nodeId, int waterbodyId, Map<BarrierOpPeriod, Integer> barrierOpTs){
+		_nodeId = nodeId;
+		_waterbodyId = waterbodyId;
+		_barrierOpTs = barrierOpTs;	
+	} 
+	public NonPhysicalBarrier(){
+		this(-1, -1, null);
+	}
+	public NonPhysicalBarrier(int nodeId, int waterbodyId){
+		_nodeId = nodeId;
+		_waterbodyId = waterbodyId;
+		_barrierOpTs = null;	
+	}
+	public void setBarrier(int nodeId, int waterbodyId, Map<BarrierOpPeriod, Integer> barrierOpTs){
+		_nodeId = nodeId;
+		_waterbodyId = waterbodyId;
+		_barrierOpTs = barrierOpTs;	
+	}
+	public void setLocations(int nodeId, int waterbodyId){
+		_nodeId = nodeId;
+		_waterbodyId = waterbodyId;
+	}
+	public void setBarrierOpTs(Map<BarrierOpPeriod, Integer> barrierOpTs){
+		_barrierOpTs = barrierOpTs;	
+	}
+	public int getNodeId(){
+		return _nodeId;
+	}
+	public int getWaterbodyId(){
+		return _waterbodyId;
+	}
+	public int getBarrierOp(Calendar currentTime){
+		BarrierOpPeriod key;
+        Iterator<BarrierOpPeriod> it = _barrierOpTs.keySet().iterator();
+        boolean found = false;
+        do{
+            key = (BarrierOpPeriod) it.next();
+            //System.out.println(key.getStartTime().getTime()+"--"+key.getEndTime().getTime());
+        }while (!(found = key.contains(currentTime))&&it.hasNext());
+        if (found)
+        	return _barrierOpTs.get(key);
+        else{
+        	System.err.println("model time:"+currentTime.getTime()
+        			+", cannot find a match in the barrier operation timeseries.");
+         	return -1;
+        }
+	}
+	protected Map<BarrierOpPeriod, Integer> getBarrierOpTs(){
+		return _barrierOpTs;
+	}
+	protected boolean setCurrentOperation(Calendar currentTime){
+		_currentOp = getBarrierOp(currentTime);
+		//System.out.println("currentOp:"+_currentOp);
+		if (_currentOp == -1)
+				return false;
+		else
+			return true;
+	}
+	protected int getCurrentOperation(){
+		return _currentOp;
+	}
+	protected void InstallBarrier(){
+		_installed = true;
+	}
+	protected boolean isInstalled(){
+		return _installed;
+	}
+	private int _nodeId;
+	private int _waterbodyId;
+	private Map<BarrierOpPeriod, Integer> _barrierOpTs;
+	private boolean _installed = false;
+	private int _currentOp = 0;
+	//private int _barrierOn = 0;
+}
