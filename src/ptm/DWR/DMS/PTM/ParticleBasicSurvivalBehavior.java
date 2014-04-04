13dbbdf7a5e49c2baf8780263d2bc83b7c57cbff
Xiaochun Wang <xwang@water.ca.gov>
2014-04-04 22:06:37 +0000

    those files should be added to previous check-ins
    git-svn-id: http://dminfo:8686/svn/repository/models/branches/ptm_development@2578 cdc4813b-4270-ec4f-936e-925f93a782c2

---
+++ src/ptm/DWR/DMS/PTM/ParticleBasicSurvivalBehavior.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package DWR.DMS.PTM;
+
+/**
+ * @author xwang
+ *
+ */
+public class ParticleBasicSurvivalBehavior implements SurvivalBehavior {
+
+	/**
+	 * 
+	 */
+	public ParticleBasicSurvivalBehavior() {
+		// TODO Auto-generated constructor stub
+		// currently doing nothing
+	}
+
+	/* (non-Javadoc)
+	 * @see DWR.DMS.PTM.SurvivalBehavior#isSurvived(DWR.DMS.PTM.Particle, float)
+	 */
+	@Override
+	public void isSurvived(Particle p, float timeToAdvance) {
+		// TODO Auto-generated method stub
+		// currently doing nothing
+
+	}
+
+}
