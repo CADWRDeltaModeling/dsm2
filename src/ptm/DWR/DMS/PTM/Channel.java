bd48dfb68d560d9c88a79f3b061a5d11a54cb32d
Xiaochun Wang <xwang@water.ca.gov>
2014-08-03 19:41:02 +0000

    particle has velocity only when flow > 0 otherwise particle velocity = 0.
    git-svn-id: http://dminfo:8686/svn/repository/models/branches/ptm_development@2652 cdc4813b-4270-ec4f-936e-925f93a782c2

--- src/ptm/DWR/DMS/PTM/Channel.java
+++ src/ptm/DWR/DMS/PTM/Channel.java
@@ -148,13 +148,13 @@ public class Channel extends Waterbody{
     float vp=1.0f, tp=1.0f;
     if(useVertProfile) vp = calcVertProfile(zPos, depth);
     if(useTransProfile) tp = calcTransProfile(yPos, width);
-    if (averageVelocity < 0)
-    	return 0;
-    else{
-    	if (_isSwimmingVelSet) 
+    if (averageVelocity > 0){
+    	if (_isSwimmingVelSet)
     		return (averageVelocity*vp*tp + _swimmingVelocity);
     	return  (averageVelocity*vp*tp + uSwimmingVelocity);
     }
+    else
+	  return 0;
   }
   
   public void setSwimmingVelocity(float sv){
