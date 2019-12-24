a954830939dfbf6f726d058c17d9d0c2830646b9
Nicky Sandhu <psandhu@water.ca.gov>
2019-12-23 22:53:08 -0800

    build file commands with setup commands at beginning

--- build_all.bat
+++ build_all.bat
@@ -1,3 +1,45 @@
-cmd /c "cd oprule & build_vs2015_32b.bat"
-cmd /c "cd input_storage & build_vs2015_32b.bat"
-cmd /c "cd dsm2 & build_dsm2_vs2015_32b.bat"
+SETLOCAL
+REM CUSTOMIZE BELOW TO MATCH YOUR ENVIRONMENT
+SET CYGWIN_DIR=D:\cygwin
+SET PYTHON_DIR=D:\Programs\Anaconda2\envs\dsm2
+SET PATH=%PYTHON_DIR%;%CYGWIN_DIR%;%PATH%
+REM 32 bit java needed
+SET JAVA_HOME="C:\Program Files (x86)\Java\jdk1.8.0_191"
+SET DSM2_THIRD_PARTY_DIR=\\cnrastore-bdo\Delta_Mod\Share\DSM2\compile_support\third_party
+CALL "c:\Program Files (x86)\IntelSWTools\compilers_and_libraries\windows\bin\compilervars.bat" ia32 vs2015
+REM BUILD OPRULE, INPUT STORAGE and then DSM2
+ECHO Start building oprule...
+cd oprule
+cmake -E remove_directory BUILD
+cmake -E make_directory BUILD
+cd BUILD
+cmake -DTHIRD_PARTY_DIR=%DSM2_THIRD_PARTY_DIR% -G "Visual Studio 14 2015" ..\ || goto :ERROR
+cmake --build . --target ALL_BUILD --config Debug || goto :ERROR
+cmake --build . --target ALL_BUILD --config Release || goto :ERROR
+cd ../..
+
+ECHO Start building input_storage...
+cd input_storage
+cmake -E remove_directory BUILD
+cmake -E make_directory BUILD
+cd BUILD
+cmake -DTHIRD_PARTY_DIR=%DSM2_THIRD_PARTY_DIR% -G "Visual Studio 14 2015" ..\|| goto :ERROR
+cmake --build . --target ALL_BUILD --config Debug || goto :ERROR
+cmake --build . --target ALL_BUILD --config Release || goto :ERROR
+cd ../..
+
+ECHO Start building DSM2...
+cd dsm2
+cmake -E remove_directory BUILD
+cmake -E make_directory BUILD
+cd BUILD
+cmake -DTHIRD_PARTY_DIR=%DSM2_THIRD_PARTY_DIR% -G "Visual Studio 14 2015" ..\src || goto :ERROR
+cmake --build . --target ALL_BUILD --config Release  || goto :ERROR
+cpack
+cd ../..
+
+
+
+:ERROR
+echo Failed with error #%ERRORLEVEL%.
+exit =b %ERRORLEVEL%
