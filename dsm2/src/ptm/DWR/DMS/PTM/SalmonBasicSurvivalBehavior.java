ef58b53bb89e7d8c1dced17498ca3996ec2fea03
Wang <Xiaochun.Wang@water.ca.gov>
2020-01-09 12:58:11 -0800

    no message

--- dsm2/src/ptm/DWR/DMS/PTM/SalmonBasicSurvivalBehavior.java
+++ dsm2/src/ptm/DWR/DMS/PTM/SalmonBasicSurvivalBehavior.java
@@ -103,8 +103,9 @@ public class SalmonBasicSurvivalBehavior implements SalmonSurvivalBehavior {
 			}
 			float t = p.age - _pStartAge.get(pId);
 			/*
-			 * From Aderson, J. J., Gurarie, E., & Zabel, R. W. (2005).  Mean free-path length theory of
-			 * predator–prey interactions: Application to juvenile salmon migration.  Ecological Modelling,
+			 * From Aderson, J. J., Gurarie, E., & Zabel, R. W. (2005).  Mean free path length theory of
+			 * predator prey interactions: Application to juvenile salmon migration.  Ecological Modelling,
+			 * 186(2), 196 211. doi:10.1016/j.ecolmodel.2005.01.014
 			 * Units of lambda are feet; units of omega are feet/sec. t in seconds and x in feet
 			 */
 			//if (_pProb.get(pId) == null)
