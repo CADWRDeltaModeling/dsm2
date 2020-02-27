96825a8f04751ae9f954483013290bd347adf530
Kijin Nam <knam@water.ca.gov>
2020-02-27 13:06:08 -0800

    Update Hydro make files
    The changes are from the master branch.

--- dsm2/src/common/version_generate.py
+++ dsm2/src/common/version_generate.py
@@ -1,5 +1,5 @@
 import os
-print __file__
+import subprocess
 
 VersionTemplate     = "      character*16 :: dsm2_version = '@{Version_GIT_DESCRIBE}', git_build = '@{Version_GIT}', git_uid = '@{GUID_GIT}'"
 VersionFile_path    = os.path.split( __file__)[0]
@@ -8,21 +8,18 @@ VersionFile_path    = os.path.join(VersionFile_path,"version.fi")
 VersionFile = open(VersionFile_path, "w")
 
 try:
-    (dummy, GITVersion_Describe) = os.popen4("git describe")
-    GITVersion_Describe = GITVersion_Describe.readlines()[0]
-    GITVersion_Describe = GITVersion_Describe.strip()
+    p = subprocess.Popen("git describe", shell=True, stdout=subprocess.PIPE, universal_newlines=True)
+    GITVersion_Describe = p.communicate()[0].strip()
 
-    (dummy, GITVersion_SourceCode) = os.popen4("git log --oneline | wc -l")
-    GITVersion_SourceCode = GITVersion_SourceCode.readlines()[0]
-    GITVersion_SourceCode = GITVersion_SourceCode.strip()
+    p = subprocess.Popen("git log --oneline -- | wc -l", shell=True, stdout=subprocess.PIPE, universal_newlines=True)
+    GITVersion_SourceCode = p.communicate()[0].strip()
 
-    (dummy, GITGUID_SourceCode) = os.popen4("git show -s --pretty=format:%h")
-    GITGUID_SourceCode = GITGUID_SourceCode.readlines()[0]
-    GITGUID_SourceCode = GITGUID_SourceCode.strip()
+    p = subprocess.Popen("git show -s --pretty=format:%h", shell=True, stdout=subprocess.PIPE, universal_newlines=True)
+    GITGUID_SourceCode = p.communicate()[0].strip()
 
-    print ' GIT description of dsm2:     '+ GITVersion_Describe
-    print ' GIT version of dsm2:     '+ GITVersion_SourceCode
-    print ' GIT guid of dsm2:        '+ GITGUID_SourceCode
+    print(' GIT description of dsm2:     '+ GITVersion_Describe)
+    print(' GIT version of dsm2:     '+ GITVersion_SourceCode)
+    print(' GIT guid of dsm2:        '+ GITGUID_SourceCode)
     VersionTxt = VersionTemplate.replace("@{Version_GIT_DESCRIBE}", GITVersion_Describe)
     VersionTxt = VersionTxt.replace("@{Version_GIT}", GITVersion_SourceCode)
     VersionTxt = VersionTxt.replace("@{GUID_GIT}", GITGUID_SourceCode)
@@ -31,4 +28,4 @@ try:
 except:
     VersionFile.close()
     os.remove(VersionFile_path)
-    print 'Abort.... possible error in file /dsm2/src/common/version_generate.py'
+    print('Abort.... possible error in file /dsm2/src/common/version_generate.py')
