863e9a4a8eb1f9d916f8541a0a94d8e87e51e0cd
Han Sang (Hans) Kim <HanSang.Kim@water.ca.gov>
2020-03-11 15:03:11 -0700

    Delete unused subdirectory

--- gtm/build_gtm_vs2015_32b.bat
+++
@@ -1,8 +0,0 @@
-setlocal
-call "c:\Program Files (x86)\IntelSWTools\compilers_and_libraries\windows\bin\compilervars.bat" ia32 vs2015
-cmake -E remove_directory BUILD
-cmake -E make_directory BUILD
-cd BUILD
-cmake -G "Visual Studio 14 2015" ..\src
-rem cmake --build . --target ALL_BUILD --config Debug
-cmake --build . --target ALL_BUILD --config Release
