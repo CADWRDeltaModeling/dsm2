38a6931a368422fbe210fe5b42b4ebf5e114fd90
Han Sang (Hans) Kim <HanSang.Kim@water.ca.gov>
2020-03-10 17:38:09 -0700

    Move DSM2-only vars and params
    At the same time, remove duplicate declaration of chan_geom;
    DSM2 uses static allocation, whereas GTM uss dynamic allocation.
    GTM's dynamic allocation is kept.

--- dsm2/src/gtm_core/common_xsect.f90
+++ dsm2/src/gtm_core/common_xsect.f90
@@ -23,8 +23,16 @@
 !>@ingroup gtm_core
 module common_xsect
 
-    use common_variables, only: n_chan, chan_geom
-    use gtm_precision
+    !@# use common_variables, only: n_chan, chan_geom	@!# This is the original line, replaced by the next:
+	use common_variables, only: n_chan !@# chan_geom is declared in module grid_data.
+	
+    !@# use gtm_precision	!@# This is the original line, replaced by the next:
+	use gtm_precision, only: gtm_real, LARGEREAL, Small, half, two	!@# some variables in gtm_precision will cause conflict with DSM2's variables.
+	
+	!@# Module grid_data is required for merging.
+	use grid_data, only: chan_geom, max_channels, &
+		nchans, levee_slope, area_tolerance, deltax_requested, xsect_geom
+        
     implicit none  
     integer :: n_irreg                         !< actual number of irregular cross sections
     integer :: max_num_elev                    !< max number of elevations in dimension                       
@@ -33,6 +41,60 @@ module common_xsect
     integer, allocatable :: num_xsect_chan(:)  !< number of xsect in a channel
     integer, allocatable :: num_elev_chan(:)   !< number of elevations in a channel
     
+    !@# vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+	!@# From DSM2's common_xsect in common_irreg.geom.f
+    integer:: &
+        max_elevations    ! maximum virtual sec in a channel
+    integer:: max_assg_sec    ! maximum virtual sec in a channel
+    integer:: max_irr_xsects    ! maximum virtual sec in a channel
+    integer:: max_layers    ! maximum virtual sec in a channel
+    integer:: max_total_elevations    ! maximum virtual sec in a channel
+    integer:: max_virt_xsects    ! maximum virtual sec in a channel
+    integer:: max_assg_virtsec    ! maximum virtual sec in a channel
+    real*8:: max_layer_height    ! max allowable dist. for moving xsect to chan end
+    real*8:: min_layer_elev      ! max allowable dist. for moving xsect to chan end
+    real*8:: max_dist_ratio      ! max allowable dist. for moving xsect to chan end
+    
+	parameter ( &
+        max_elevations=300 &
+        ,max_assg_sec=22 &
+        ,max_assg_virtsec=15 &
+        ,max_irr_xsects=2000 &
+        ,max_layers=100000 &
+        ,max_total_elevations=20000 &
+        ,max_layer_height=100.0 &
+        ,min_layer_elev=-100.0 &
+        ,max_virt_xsects=5000 &
+        ,max_dist_ratio=0.05 &
+        )
+        
+    logical:: repl              ! if true, then readirreg will replace a
+                              ! cross-section with adj sec if adj node has 2 chan
+    parameter (repl = .false.)
+
+    integer:: nirg=0         ! actual number of irregular cross sections
+
+    integer :: total_virt_xsect  ! total entries in virtual xsect table
+    
+    !-----arrays for virtual cross-sections to replace structure which was to large.
+    !-----chan_index and num_layers will be used to calculate index of real arrays below
+    !@# chan_index is declared above as allocatable.
+    !@# integer*4:: &
+    !@#    chan_index(max_channels)
+    integer*4:: num_virt_sec(max_channels)
+    integer*4:: num_layers(max_channels)
+    integer*4:: elev_index(max_channels)
+    integer*4:: minelev_index(max_virt_xsects)
+    real*8:: &
+        virt_area(max_layers)
+    real*8:: virt_wet_p(max_layers)
+    real*8:: virt_width(max_layers)
+    real*8:: virt_z_centroid(max_layers)
+    real*8:: virt_elevation(max_total_elevations)
+    real*8:: virt_min_elev(max_virt_xsects)
+    real*8:: virt_deltax(max_channels)
+    !@# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+        
     !> Define type for virtual xsect  
     type cross_section_t
         real(gtm_real) :: min_elev                       !< minimum elevation in the sec
@@ -60,6 +122,24 @@ module common_xsect
     end type
     
     type(cross_section_t), allocatable:: virt_xsect(:)
+    !@# vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+    !@# Below: from DSM2 (module common_xsect in common_irreg_geom.f).
+    type(cross_section_t):: irreg_geom(0:max_irr_xsects)
+
+    type xsect_assg_t
+        !--------cross-section assignment structure.  index of sec_index
+        !--------is the irreg. section number, which is also the index
+        !--------of the irreg_geom structure
+        integer sec_index(max_assg_sec) ! xsect number (index of irreg_geom str)
+        integer num_sec_orig   ! number of sec originally assigned
+        integer num_sec_assg   ! number of sec assigned after copying
+        logical original(max_assg_sec) ! true if the section is original (not copy)
+        logical rect(max_assg_sec) ! true if the section is rectangular
+        real*8 dist(max_assg_sec)
+    end type
+
+    type(xsect_assg_t):: xsect_assg(0:max_channels)
+    !@# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
     
     contains
     
