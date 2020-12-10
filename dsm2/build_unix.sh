56b9cbd10a27941d9c52821c8a612342cc37370c
Nicky Sandhu <psandhu@water.ca.gov>
2020-12-10 10:57:38 -0800

    for linux build: adding jdk6 and 19.0.5 compiler to match windows version

--- dsm2/build_unix.sh
+++ dsm2/build_unix.sh
@@ -5,6 +5,6 @@ cmake -E make_directory BUILD
 cd BUILD
 cmake -G "Unix Makefiles" ../src
 #cmake --build . --target ALL_BUILD --config Debug
-#cmake --build . --target ALL_BUILD --config Release
+cmake --build . --target ALL_BUILD --config Release
 cmake --build .
-#cpack
+cpack
