693e97811b6ddfb6c0083bb1981bc876dc4a9d46
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-14 01:09:34 +0800

    Delete input_storage directory

--- input_storage/src/__init__.py
+++
@@ -1,3 +0,0 @@
-"""
-/** \defgroup gen Python Code Generation Script*/
-"""
--- input_storage/src/exception_trapping.cpp
+++
@@ -1,8 +0,0 @@
-#include "exception_trapping.h"
-
-#include <iostream>
-#include <string>
-void input_storage_log_err_f(const std::string & message)
-{
-   std::cerr << message << std::endl;
-}
