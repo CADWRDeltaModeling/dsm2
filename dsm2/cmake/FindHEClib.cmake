664c840bd9a2679d3fb43a0673a6ad887e529a72
Kijin Nam <knam@water.ca.gov>
2023-10-02 19:39:44 -0700

    CMake updates for Windows
    This commit is a round trip to accept CMake setting changes from Linux.
    Now Windows CMake files use CMake conventions, for example,
    find_package. A sample CMake setting file, win_options.cmake, is added
    as well.

--- dsm2/cmake/FindHEClib.cmake
+++ dsm2/cmake/FindHEClib.cmake
@@ -12,14 +12,22 @@ else()
   set(_HEClib_SEARCH_OPTS)
 endif()
 
+if(WIN32)
+  set(LIB_PREFIX "")
+  set(LIB_SUFFIX ".lib")
+else()
+  set(LIB_PREFIX "lib")
+  set(LIB_SUFFIX ".a")
+endif()
+
 if(NOT HEClib_FOUND)
-  set(THIS_LIBRARY_SEARCH_DEBUG libheclibd.a)
+  set(THIS_LIBRARY_SEARCH_DEBUG ${LIB_PREFIX}heclibd${LIB_SUFFIX})
   find_library(
     HEClib_LIBRARY_DEBUG
     NAMES ${THIS_LIBRARY_SEARCH_DEBUG}
     HINTS ${HEClib_ROOT}
     PATH_SUFFIXES lib Lib)
-  set(THIS_LIBRARY_SEARCH_RELEASE libheclib.a)
+  set(THIS_LIBRARY_SEARCH_RELEASE ${LIB_PREFIX}heclib${LIB_SUFFIX})
   find_library(
     HEClib_LIBRARY_RELEASE
     NAMES ${THIS_LIBRARY_SEARCH_RELEASE}
---
+++ win_options.cmake
@@ -0,0 +1,36 @@
+set(JAVA_HOME
+    "C:/Program Files/Java/jdk1.6.0_45"
+    CACHE PATH "Java home")
+set(CMAKE_C_COMPILER
+    icc
+    CACHE STRING "C compiler")
+set(CMAKE_CXX_COMPILER
+    icpc
+    CACHE STRING "C++ compiler")
+set(CMAKE_Fortran_COMPILER
+    ifort
+    CACHE STRING "Fortran compiler")
+
+# set(CMAKE_CXX_STANDARD 17 CACHE STRING "C++ standard")
+set(CMAKE_POSITION_INDEPENDENT_CODE
+    ON
+    CACHE BOOL "Position independent code")
+
+set(THIRD_PARTY_DIR
+    "C:/Users/foobar/third_party"
+    CACHE PATH "Third party directory")
+set(BOOST_ROOT
+    "${THIRD_PARTY_DIR}/boost-1.83.0"
+    CACHE PATH "Boost root directory")
+set(HDF5_ROOT
+    "${THIRD_PARTY_DIR}/hdf5-1.10.10"
+    CACHE PATH "HDF5 root directory")
+set(HEClib_ROOT
+    "${THIRD_PARTY_DIR}/heclib-6"
+    CACHE PATH "HEClib root directory")
+set(KLU_ROOT
+    "${THIRD_PARTY_DIR}/suitesparse-4.0.2"
+    CACHE PATH "KLU root directory")
+set(Loki_ROOT
+    "${THIRD_PARTY_DIR}/loki-0.1.7"
+    CACHE PATH "Loki root directory")
