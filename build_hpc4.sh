a68492a5a9769bdd60b4e9ba9ec146fa628ecf55
Kijin Nam <knam@water.ca.gov>
2023-11-08 19:11:08 -0800

    Update a script for HPC4

--- build_hpc4.sh
+++ build_hpc4.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 module purge
 module load gcc/6.1.0
 module load compiler/2022.1.0
@@ -6,6 +6,6 @@ module load cmake/3.21.1
 cmake -E remove_directory build
 cmake -E make_directory build
 cd build
-cmake -C ../hpc4options.cmake -G "Unix Makefiles" \
+cmake -C ../linux_options.cmake -G "Unix Makefiles" \
 -DCMAKE_BUILD_TYPE:STRING=Debug ..
 cmake --build . --target all -j $(nproc)
