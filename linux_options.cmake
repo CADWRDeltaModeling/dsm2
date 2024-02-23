47c6b1fb76a491ed6ff98d5bc3ee0433462adabd
Kijin Nam <knam@water.ca.gov>
2024-02-23 12:04:42 -0800

    Add traceback option by default
    This commit adds an compiler option, -traceback, to show where it
    crashes when it happens.
    
    DSM2-906

--- linux_options.cmake
+++ linux_options.cmake
@@ -15,7 +15,7 @@ set(CMAKE_POSITION_INDEPENDENT_CODE
     ON
     CACHE BOOL "Position independent code")
 set(CMAKE_Fortran_FLAGS_INIT
-    "-Dhydro_1000"
+    "-Dhydro_1000 -traceback"
     CACHE STRING "Fortran flags to use a large array for the dense dx")
 
 set(BOOST_ROOT
