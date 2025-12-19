34a82e48c3d8590641c15cb15b34c9c70488e948
Kijin Nam <knam@water.ca.gov>
2025-12-19 14:43:50 -0800

    Add missing changes to sed part
    This commit adds a few missing code reorg changes to the sediment part
    of the codes. This addresses error 6580, "name in only-list does not
    exist".

--- dsm2/src/sediment_bed_setup/sb_subs.f90
+++ dsm2/src/sediment_bed_setup/sb_subs.f90
@@ -1,8 +1,6 @@
 module sb_subs
     !use constants
     use sb_common
-    !use gtm_subs, only: get_survey_top
-    !use common_vars, only: chan_geom, segm
     use hdf_util !, only: get_int_attribute_from_hdf5, n_chan, n_xsect, n_comp
     use common_xsect
     implicit none
@@ -145,7 +143,6 @@ module sb_subs
     subroutine get_survey_top(wet_perim,   &
                               elev,   &
                               ncell)
-        use common_vars, only: chan_geom, n_chan, dx_arr
         use common_xsect
         implicit none
         integer, intent(in) :: ncell
