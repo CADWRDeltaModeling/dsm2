2ad25990d77d32b95279f833e4a2cf53acdf51be
Yujia Cai <Yujia.Cai@water.ca.gov>
2025-12-22 12:10:12 -0800

    Update building setting
    1. Update to use intel 2025 compiler
    2. Build all modules

--- build_hpc4.sh
+++ build_hpc4.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 module purge
-module load intel/2024.1
+module load intel/2025.1
 module load cmake/3.29.3
 cmake -E remove_directory build
 cmake -E make_directory build
 pushd build
 cmake -C ../linux_options.cmake -G "Unix Makefiles" \
 -DCMAKE_BUILD_TYPE:STRING=Debug ..
-cmake --build . --target hydro_gtm -j $(nproc)
+cmake --build . --target all -j $(nproc)
 cpack -G "TGZ"
 popd
